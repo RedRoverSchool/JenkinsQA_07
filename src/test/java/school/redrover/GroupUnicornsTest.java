32459090e657ea5822acebafbba1b9ad92b15a08
PeterKuzyk <p.kuzyk@yahoo.com>
2023-10-04 17:07:38 -0400

    added usPsPageOpenTest

--- src/test/java/school/redrover/GroupUnicornsTest.java
+++ src/test/java/school/redrover/GroupUnicornsTest.java
@@ -1,4 +1,21 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 public class GroupUnicornsTest {
+
+    @Test
+    public void usPsPageOpenTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.usps.com/");
+        String title = driver.getTitle();
+        assertEquals("Welcome | USPS", title);
+        driver.quit();
+    }
 }
