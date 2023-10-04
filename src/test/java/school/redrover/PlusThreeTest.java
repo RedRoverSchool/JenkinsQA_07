8f780bac7de852a89121c3145d4496fff8972934
karamelev <karamelev@tut.by>
2023-10-04 22:16:35 +0300

    add Webdriver

--- src/test/java/school/redrover/PlusThreeTest.java
+++ src/test/java/school/redrover/PlusThreeTest.java
@@ -1,5 +1,9 @@
 package school.redrover;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PlusThreeTest {
+    WebDriver driver = new ChromeDriver();
     
 }
