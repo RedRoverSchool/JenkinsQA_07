0165f4c84e70af19f68289065bae0ca83721a055
tryam <tryamkin@gmail.com>
2023-10-05 18:28:31 +0300

    correct classname

--- src/test/java/school/redrover/SevchenkoTest.java
+++ src/test/java/school/redrover/SevchenkoTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 
 
 public class SevchenkoTest {
+
     //Search elements //*[@id='mainbar']//a[@href='/questions/tagged/slf4j']
     //Show elements//div[@class='s-pagination site1 themed page-sizer float-right']//a[.='15']
 
