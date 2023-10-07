e686b2057415d89efd25b7f5e160d78c9aee4fd6
Olena Kharkova <helen.kha.ua@gmail.com>
2023-10-07 19:56:25 +0300

    add Lena's test into group tests_1

--- src/test/java/school/redrover/GroupItFriendlyTest.java
+++ src/test/java/school/redrover/GroupItFriendlyTest.java
@@ -201,7 +201,6 @@ public class GroupItFriendlyTest {
             WebElement checkboxIsSelected = driver.findElement(By.xpath("//span[@class=\"rct-checkbox\"]"));
             checkboxIsSelected.click();
 
-
             assertEquals(driver.findElement(By.xpath("//*[@id=\"result\"]")).getText(), "You have selected :\n" +
                     "home\n" +
                     "desktop\n" +
