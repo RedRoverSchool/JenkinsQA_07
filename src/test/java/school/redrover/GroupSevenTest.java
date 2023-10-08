b29430130fd1ff47ae379d718260f44f04037d07
ArtemRazdymakha <razdymakha91@mail.ru>
2023-10-07 21:54:28 -0400

    First push

--- src/test/java/school/redrover/GroupSevenTest.java
+++ src/test/java/school/redrover/GroupSevenTest.java
@@ -8,41 +8,30 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class GroupSevenTest {
     @Test
-    public void kylieTitleTest() {
-        WebDriver driver = new ChromeDriver();
+    public void YMCA_Test() {
 
-        driver.get("https://kyliecosmetics.com/");
-        String title = driver.getTitle();
-        Assert.assertEquals(title, "Kylie Cosmetics by Kylie Jenner | Kylie Skin | Kylie Baby");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://ymcacapecod.org/");
+
+        WebElement textBox = driver.findElement(By.className("field"));
+        WebElement SearchButton = driver.findElement(By.className("submit"));
+
+        textBox.sendKeys("pool");
+        SearchButton.click();
+
+        WebElement findelement = driver.findElement(By.xpath("//*[@id=\"folio\"]/nav/ul/li[2]/a"));
+        findelement.click();
+
+        WebElement text = driver.findElement(By.xpath("//*[@id=\"content\"]/article/p[4]/strong/a"));
+        text.click();
+
+        String value = text.getText();
+        Assert.assertEquals( value,"CLICK HERE TO REGISTER ONLINE!");
 
         driver.quit();
-    }
-
-    @Test
-    public void searchTest() throws InterruptedException{
-        WebDriver driver = new FirefoxDriver();
-        try {
-            driver.get("https://kyliecosmetics.com/collections/kylie-cosmetics");
-
-            Thread.sleep(1000);
-
-            WebElement searchButton = driver.findElement(By.xpath("//a[@title='Search']"));
-            searchButton.click();
-
-            WebElement searchInput = driver.findElement(By.xpath("//input[@id='SearchForm-Header-Query']"));
-            searchInput.sendKeys("lip kit");
-
-            WebElement searchButtonNext = driver.findElement(By.xpath("//button[@id='SearchForm-Header-Submit']"));
-            searchButtonNext.click();
-
-            WebElement title = driver.findElement(By.xpath("//h1[normalize-space()='Search']"));
-            String value = title.getText();
-            Assert.assertEquals(value, "SEARCH");
-
-        } finally {
-            driver.quit();
-        }
     }
 }
