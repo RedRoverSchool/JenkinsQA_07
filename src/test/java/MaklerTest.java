2a2ad3d63e5f3eaad19e5b7cc8161435e180647c
Arnaut Natalia <nechitnatlia1@gmail.com>
2023-10-09 08:49:57 +0200

    Refactoring

--- src/test/java/MaklerTest.java
+++ src/test/java/MaklerTest.java
@@ -7,27 +7,25 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class MaklerTEST {
+public class MaklerTest {
 
     @Test
         public void testSearch() throws InterruptedException {
             WebDriver driver = new ChromeDriver();
             driver.get("https://makler.md/");
 
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(9000));
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
 
             WebElement textBox = driver.findElement(By.className("disable-border"));
             textBox.sendKeys("Handmade");
 
-            Thread.sleep(9000);
-
             WebElement searchButton = driver.findElement(By.id("header_searchButton"));
             searchButton.click();
 
+            Thread.sleep(3000);
             WebElement title = driver.findElement(By.id("header_searchField"));
             String value = title.getText();
             Assert.assertEquals(value,"Handmade");
-
             driver.quit();
         }
 }
