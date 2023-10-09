c2bdce84db6f6f53c48934d27ba4f45c64488656
PavTomak <tomak.pav@gmail.com>
2023-10-09 16:28:46 -0400

    Refactoring

---
+++ src/test/java/school/redrover/PavTomakTest.java
@@ -0,0 +1,58 @@
+package school.redrover;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class PavTomakTest {
+
+
+    @Test
+    public void typeIn() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://ymcacapecod.org/");
+
+
+        WebElement textBox = driver.findElement(By.className("field"));
+        textBox.sendKeys("Eastham");
+
+        Thread.sleep(1000);
+
+        WebElement searchButton = driver.findElement(By.className("submit"));
+        searchButton.click();
+
+        WebElement title = driver.findElement(By.className("breadcrumb"));
+        String value = title.getText();
+        Assert.assertEquals(value, "You Are Here: YMCA Cape Cod > Search results for 'Eastham'");
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void MacsShack() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://macsseafood.com/restaurant/macs-shack-wellfleet/");
+
+        WebElement viewMenu = driver.findElement(By.xpath("//*[@id=\"menu-link\"]/span[2]"));
+        viewMenu.click();
+
+        WebElement orderOnline = driver.findElement(By.xpath("//*[text() = 'Order Online']"));
+        orderOnline.click();
+
+
+
+        driver.quit();
+
+
+    }
+}
+
+
