a3354c1d8a5efbcdcad924791e8bb2fbf0b5ce52
AnzhelikaLa <aalashko@gmail.com>
2023-10-10 16:18:52 -0500

    Amazon test change (added Assert)

--- src/test/java/school/redrover/AnzhAmazonTest.java
+++ src/test/java/school/redrover/AnzhAmazonTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AnzhAmazonTest {
@@ -22,20 +23,20 @@ public class AnzhAmazonTest {
 
             searchBox.sendKeys("Selenium WebDriver");
 
-
             searchBox.submit();
 
-
             Thread.sleep(2000);
+
+            WebElement titleElement = driver.findElement(By.xpath("//span[contains(text(), 'Results')]"));
+            String value = titleElement.getText();
+
+            Assert.assertTrue(value.contains("Results"));
 
 
         } finally {
            driver.quit();
         }
     }
-
-
-
 
 }
 
