94ff71e1f84abf0511ee434e5e7fd890406f71a0
ppashchenkov <ppashchenkov@gmail.com>
2023-10-11 21:51:49 +0300

    refactoring

--- src/test/java/school/redrover/GroupLetsQATest.java
+++ src/test/java/school/redrover/GroupLetsQATest.java
@@ -2,7 +2,6 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
@@ -10,6 +9,7 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class GroupLetsQATest extends BaseTest {
+
     private static final String BASE_URL = "https://www.sawinery.net/";
     @Ignore
     @Test
@@ -47,4 +47,30 @@ public class GroupLetsQATest extends BaseTest {
         Assert.assertFalse("rgba(0, 123, 255, 1)".equals(badButton.getCssValue("background-color")));
     }
 
+    @Ignore
+    @Test
+    public void verifyTextTest() {
+        getDriver().get("http://www.uitestingplayground.com/verifytext");
+
+        WebElement textBlock = getDriver().findElement(By.xpath("//html/body/section/div/div[4]/span"));
+        String text = textBlock.getText();
+
+        Assert.assertEquals(text, "Welcome UserName!");
+    }
+
+    @Ignore
+    @Test
+    public void testConfIxbt(){
+
+        getDriver().get("https://www.ixbt.com/");
+
+        String title = getDriver().getTitle();
+        Assert.assertEquals("Новости технологий, обзоры гаджетов, смартфонов, бытовой техники и автомобилей", title);
+
+        WebElement conference = getDriver().findElement(By.linkText("Конференция"));
+        conference.click();
+
+        String title_k = getDriver().getTitle();
+        Assert.assertEquals("Конференция iXBT.com", title_k);
+    }
 }
