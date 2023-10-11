7e91678238f9220cce5d827ddbf58ca7f9106cb6
FilipR <fififima92@gmail.com>
2023-10-11 19:39:40 +0200

    parent class removed 'try' construction

--- src/test/java/school/redrover/runner/BaseTest.java
+++ src/test/java/school/redrover/runner/BaseTest.java
@@ -1,7 +1,5 @@
 package school.redrover.runner;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
@@ -14,20 +12,20 @@ import java.time.Duration;
 @Listeners({FilterForTests.class})
 public abstract class BaseTest {
 
-    private WebDriver driver;
+    private WebDriver webDriver;
 
     @BeforeMethod
     protected void beforeMethod(Method method) {
-        driver = new ChromeDriver(ProjectUtils.chromeOptions);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriver = new ChromeDriver(ProjectUtils.chromeOptions);
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterMethod
     protected void afterMethod(Method method, ITestResult testResult) {
-        driver.quit();
+        webDriver.quit();
     }
 
     protected WebDriver getDriver() {
-        return driver;
+        return webDriver;
     }
 }
