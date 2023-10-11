270c1864a07d7d39b966d61ae374d4569ecb80c3
Maria Dymsha <dymsha.maria@mail.ru>
2023-10-11 13:56:38 +0300

    add new test

---
+++ src/test/java/school/redrover/MariaTest.java
@@ -0,0 +1,35 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class MariaTest {
+
+    @Test
+    public void testSearchMD() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://www.google.com/");
+
+            WebElement textBox = driver.findElement(By.className("gLFyf"));
+            textBox.sendKeys("Selenium");
+
+            Thread.sleep(1000);
+
+            WebElement searchButton = driver.findElement(By.className("gNO89b"));
+            searchButton.click();
+
+            WebElement title = driver.findElement(By.xpath("//div/span[@class = \"VuuXrf\"]"));
+            String value = title.getText();
+            Assert.assertEquals("Selenium", value);
+        } finally {
+            driver.quit();
+        }
+    }
+}
