f5f428f8aea3c4ae158b5600437ee37cd9141e7b
ArturSabanadze <Diakosmisi26@hotmail.com>
2023-10-11 17:08:59 +0200

    ArSaFirstTest extends BaseTest

--- src/test/java/school/redrover/ArSaFirstTest.java
+++ src/test/java/school/redrover/ArSaFirstTest.java
@@ -1,96 +1,72 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
-public class ArSaFirstTest {
+public class ArSaFirstTest extends BaseTest {
 
     //Check if Title of the Website is correct..
     @Test
     public void titleCheck()
     {
+            getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
 
-        WebDriver driver = new ChromeDriver();
-        try
-        {
-            driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-            String title = driver.getTitle();
+            String title = getDriver().getTitle();
             Assert.assertEquals(title, "Web form");
-        }
-        finally {
-            driver.quit();
-        }
     }
     //Check if the form "Text Input" is working, try to input some text, check the input text.
     @Test
     public void textInputTest() {
 
-        WebDriver driver = new ChromeDriver();
-        try{
-            driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+            getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
 
-            WebElement nameInput = driver.findElement(By.id("my-text-id"));
+            WebElement nameInput = getDriver().findElement(By.id("my-text-id"));
             nameInput.sendKeys("Artur Sabanadze");
 
             String enteredText = nameInput.getAttribute("value");
             Assert.assertEquals(enteredText, "Artur Sabanadze");
         }
-        finally {
-            driver.quit();
-        }
-    }
 
     //Check "my-disabled" element for the following functions: is disabled, is visible and the placeholder text is correct.
     @Test
     public void disabledTextInputTest() {
-        WebDriver driver = new ChromeDriver();
-        try {
-            driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-            WebElement disabledInput = driver.findElement(By.name("my-disabled"));
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
 
-            // Check if the input is disabled
-            Assert.assertEquals(disabledInput.getAttribute("disabled"), "true");
+        WebElement disabledInput = getDriver().findElement(By.name("my-disabled"));
 
-            //Check if the element is visible
-            Assert.assertTrue(disabledInput.isDisplayed());
+        // Check if the input is disabled
+        Assert.assertEquals(disabledInput.getAttribute("disabled"), "true");
 
-            // Check the placeholder text in the disabled input
-            String placeholderText = disabledInput.getAttribute("placeholder");
-            Assert.assertEquals(placeholderText, "Disabled input");
+        //Check if the element is visible
+        Assert.assertTrue(disabledInput.isDisplayed());
 
-        } finally {
-            driver.quit();
-        }
+        // Check the placeholder text in the disabled input
+        String placeholderText = disabledInput.getAttribute("placeholder");
+        Assert.assertEquals(placeholderText, "Disabled input");
+
+
     }
-
 
     @Test
     public void colorCheck() {
-        WebDriver driver = new ChromeDriver();
-        try {
 
-            driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
 
 
-            WebElement colorInputElement = driver.findElement(By.name("my-colors"));
+        WebElement colorInputElement = getDriver().findElement(By.name("my-colors"));
 
 
-            String actualColor = colorInputElement.getAttribute("value");
+        String actualColor = colorInputElement.getAttribute("value");
 
 
-            String expectedColor = "#563d7c";
+        String expectedColor = "#563d7c";
 
 
-            Assert.assertEquals(actualColor, expectedColor, "Element color is not as expected");
-        }
-        finally {
-            driver.quit();
-        }
+        Assert.assertEquals(actualColor, expectedColor, "Element color is not as expected");
+
     }
 }
