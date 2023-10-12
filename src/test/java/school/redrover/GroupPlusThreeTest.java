b1e2f630ef6f9c14f378c8041860bbb2d6ed6311
SawkaQA <aleksandr.sdobnov.qa@gmail.com>
2023-10-12 20:45:27 +0300

    ref classname

--- src/test/java/school/redrover/GroupPlusThreeTest.java
+++ src/test/java/school/redrover/GroupPlusThreeTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
-public class PlusThreeTest extends BaseTest {
+public class GroupPlusThreeTest extends BaseTest {
 
     public static final String USERNAME = "TestUser1";
     public static final String URL = "https://parabank.parasoft.com/parabank/register.htm";
@@ -60,7 +60,7 @@ public class PlusThreeTest extends BaseTest {
         loginButton.click();
     }
     
-    @Test(dataProviderClass = PlusThreeTest.DataProviders.class, dataProvider = "validData")
+    @Test(dataProviderClass = GroupPlusThreeTest.DataProviders.class, dataProvider = "validData")
     public void TestAuthorizationPositive(String username, String password) {
 
         login(username, password);
@@ -70,7 +70,7 @@ public class PlusThreeTest extends BaseTest {
 
     }
 
-    @Test(dataProviderClass = PlusThreeTest.DataProviders.class, dataProvider = "invalidData")
+    @Test(dataProviderClass = GroupPlusThreeTest.DataProviders.class, dataProvider = "invalidData")
     public void TestAuthorizationNegative(String username, String password) {
 
         login(username, password);
@@ -80,7 +80,7 @@ public class PlusThreeTest extends BaseTest {
 
     }
 
-    @Test(description = "Add to cart via catalog", dataProviderClass = PlusThreeTest.DataProviders.class, dataProvider = "validData")
+    @Test(description = "Add to cart via catalog", dataProviderClass = GroupPlusThreeTest.DataProviders.class, dataProvider = "validData")
     public void TestAddCart(String username, String password) {
 
         login(username, password);
@@ -94,7 +94,7 @@ public class PlusThreeTest extends BaseTest {
 
     }
 
-    @Test(description = "Remove from cart via basket", dataProviderClass = PlusThreeTest.DataProviders.class, dataProvider = "validData")
+    @Test(description = "Remove from cart via basket", dataProviderClass = GroupPlusThreeTest.DataProviders.class, dataProvider = "validData")
     public void TestRemoveCart(String username, String password) {
 
         login(username, password);
