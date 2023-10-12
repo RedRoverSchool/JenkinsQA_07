878d88494b7202e03300d3907b9cc9c55bda9a56
n-stepanova <nstepanovaqa@gmail.com>
2023-10-12 22:43:35 +0300

    remove exception from method signature

--- src/test/java/school/redrover/GroupIntroVertsQaTest.java
+++ src/test/java/school/redrover/GroupIntroVertsQaTest.java
@@ -1,6 +1,5 @@
 package school.redrover;
 
-import org.apache.commons.compress.utils.ByteUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
