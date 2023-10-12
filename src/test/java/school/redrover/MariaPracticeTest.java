100171b89f827eeb10d11c62c95ec23ec5926a83
Maria Dymsha <dymsha.maria@mail.ru>
2023-10-12 13:50:05 +0300

    RefactoringSearchTest

--- src/test/java/school/redrover/MariaPracticeTest.java
+++ src/test/java/school/redrover/MariaPracticeTest.java
@@ -1,38 +1,29 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-import java.time.Duration;
 
 public class MariaPracticeTest extends BaseTest {
 
     @Test
     public void SearchTest()  {
 
-        WebDriver driver = new ChromeDriver();
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-        String title = driver.getTitle();
+        String title = getDriver().getTitle();
         Assert.assertEquals("Web form", title);
 
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        WebElement textBox = getDriver().findElement(By.name("my-text"));
+        WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
 
         textBox.sendKeys("Selenium");
         submitButton.click();
 
-        WebElement message = driver.findElement(By.id("message"));
+        WebElement message = getDriver().findElement(By.id("message"));
         String value = message.getText();
         Assert.assertEquals("Received!", value);
-
-        driver.quit();
     }
 }
