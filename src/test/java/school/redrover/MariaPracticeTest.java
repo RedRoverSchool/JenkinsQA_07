68721d4c594e60e410ff3d4e17cd02fed0338da7
Maria Dymsha <dymsha.maria@mail.ru>
2023-10-12 12:52:33 +0300

    Refactoring practice test

--- src/test/java/school/redrover/MariaPracticeTest.java
+++ src/test/java/school/redrover/MariaPracticeTest.java
@@ -5,35 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
 
 public class MariaPracticeTest extends BaseTest {
-    WebDriver driver = new ChromeDriver();
-
-    @BeforeMethod
-    public void getPage(){
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-    }
-
-    @AfterMethod
-    public void closeChrome(){
-        driver.quit();
-    }
 
     @Test
     public void SearchTest()  {
 
-//            driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
         String title = driver.getTitle();
         Assert.assertEquals("Web form", title);
-
 
         WebElement textBox = driver.findElement(By.name("my-text"));
         WebElement submitButton = driver.findElement(By.cssSelector("button"));
@@ -44,5 +32,7 @@ public class MariaPracticeTest extends BaseTest {
         WebElement message = driver.findElement(By.id("message"));
         String value = message.getText();
         Assert.assertEquals("Received!", value);
+
+        driver.quit();
     }
 }
