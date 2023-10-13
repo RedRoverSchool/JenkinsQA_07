23438342c81b9e1402af71acd3efdd9e778c4d2b
yanasaa <yanasaa@gmail.com>
2023-10-13 19:12:22 +0300

    rename test added, add Test Commit, Ignore class

--- src/test/java/school/redrover/GroupPlusThreeTest.java
+++ src/test/java/school/redrover/GroupPlusThreeTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
+//import school.redrover.runner.JenkinsUtils;
 
 import java.time.Duration;
 import java.util.List;
