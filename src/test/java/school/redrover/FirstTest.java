5ef0e021dc4923b1ca272dd45b63fdb2860d7bbc
AkikoAoki <iakikoaokii@gmail.com>
2023-10-13 17:19:37 +0400

    change name and ignore FirstTest

--- src/test/java/school/redrover/FirstTest.java
+++ src/test/java/school/redrover/FirstTest.java
@@ -2,11 +2,12 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.JenkinsUtils;
 
-
+@Ignore
 public class  FirstTest extends BaseTest {
 
     @Test
