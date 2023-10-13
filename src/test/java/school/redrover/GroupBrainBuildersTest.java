58f1f36e4ad8b14e53f7f10e74ea04c753daa681
realandreyantropov <ramchem1993@gmail.com>
2023-10-13 16:19:19 +0300

    changing class name for group

--- src/test/java/school/redrover/GroupBrainBuildersTest.java
+++ src/test/java/school/redrover/GroupBrainBuildersTest.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 import static org.testng.Assert.assertEquals;
 
 
-public class BrainBuildersTest extends BaseTest {
+public class GroupBrainBuildersTest extends BaseTest {
 
     @Ignore
     @Test
