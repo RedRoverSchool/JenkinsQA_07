8ece7a08e82f3f5d4d558de2861825c4a2b9962a
Solyris_Rus <Svet2603@mail.ru>
2023-10-14 23:48:54 +0300

    Refactoring my test @Ignore

--- src/test/java/school/redrover/GroupUnitedByJavaTest.java
+++ src/test/java/school/redrover/GroupUnitedByJavaTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import school.redrover.runner.BaseTest;
