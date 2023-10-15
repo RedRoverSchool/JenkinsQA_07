365a663b048ece28b7f939d040f9234d47800c42
Lifantsova <lifancova@mail.ru>
2023-10-16 00:01:20 +0400

    Refactoring

--- src/test/java/school/redrover/LifantsovaTest.java
+++ src/test/java/school/redrover/LifantsovaTest.java
@@ -1,15 +1,10 @@
 package school.redrover;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
 
 public class LifantsovaTest extends BaseTest {
 
