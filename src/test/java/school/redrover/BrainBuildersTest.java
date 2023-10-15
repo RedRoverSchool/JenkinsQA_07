707e86232dde6842efe00590dfa2e3a406d23e7a
realandreyantropov <ramchem1993@gmail.com>
2023-10-15 08:05:32 +0300

    refactoring 2.0

--- src/test/java/school/redrover/BrainBuildersTest.java
+++ src/test/java/school/redrover/BrainBuildersTest.java
@@ -92,26 +92,4 @@ public class BrainBuildersTest extends BaseTest {
             driver.quit();
         }
     }
-
-    @Test
-    public void testAskentSearch() {
-            getDriver().get("https://www.askent.ru/");
-
-            String title = getDriver().getTitle();
-            assertEquals(title, "ASKENT - российский бренд аксессуаров из натуральной кожи");
-
-            WebElement magnifierIcon = getDriver().findElement(By.className("search_icon"));
-            magnifierIcon.click();
-
-            WebElement searchTextField = getDriver().findElement(By.name("q"));
-            searchTextField.click();
-            searchTextField.sendKeys("сумка");
-
-            WebElement magnifierButton = getDriver().findElement(By.xpath("//button[@type='submit']"));
-            magnifierButton.click();
-
-            WebElement searchResult = getDriver().findElement(By.cssSelector("h1"));
-            String result = searchResult.getText();
-            Assert.assertEquals(result, "Результаты поиска");
-    }
 }
