7c482a0c5e38b0c05777fcc36433a37820ed514e
vfokichev2022 <vfokichev.qa@gmail.com>
2023-10-15 17:01:46 +0300

    refactoring first test

--- src/test/java/school/redrover/FokichevTest.java
+++ src/test/java/school/redrover/FokichevTest.java
@@ -1,10 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -13,6 +10,7 @@ import static org.testng.Assert.assertEquals;
 public class FokichevTest extends BaseTest {
 
     private static final String PAGE_URL = "https://www.emu-land.net/";
+
     @Test
     public void searchTest() throws InterruptedException {
         getDriver().get(PAGE_URL);
