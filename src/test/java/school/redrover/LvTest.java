f53a228251dcde3e7c070866048a262a5ac56ecc
lazval <ll97@mail.ru>
2023-10-16 15:45:27 +0300

    first test

---
+++ src/test/java/school/redrover/LvTest.java
@@ -0,0 +1,20 @@
+package school.redrover;
+
+import school.redrover.runner.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+import school.redrover.runner.JenkinsUtils;
+
+public class LvTest extends BaseTest {
+    @Test
+    public void testSearch() throws InterruptedException {
+
+        JenkinsUtils.login(getDriver());
+
+        Assert.assertEquals(
+                getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText(),
+                "Добро пожаловать в Jenkins!");
+    }
+}
