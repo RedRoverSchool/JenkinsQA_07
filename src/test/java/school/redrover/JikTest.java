6649e93d2a958d90c0d3726f836732322b196b81
jikamad <99354694+jikamad@users.noreply.github.com>
2023-10-16 07:33:24 -0400

    Refactoring

--- src/test/java/school/redrover/JikTest.java
+++ src/test/java/school/redrover/JikTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class JikTest extends BaseTest {
+public class JikTest {
 
     @Test
     public void testTitleCheck() {
