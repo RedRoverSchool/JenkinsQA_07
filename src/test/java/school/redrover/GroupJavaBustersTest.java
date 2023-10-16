0133cef58bb5471c4021fb3d6f21843ed6ab381d
denys.sennikov <denys.sennikov@creatoriq.com>
2023-10-16 16:03:39 +0200

    - Remove annotation - Remove waiter

--- src/test/java/school/redrover/GroupJavaBustersTest.java
+++ src/test/java/school/redrover/GroupJavaBustersTest.java
@@ -4,16 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import school.redrover.runner.JenkinsUtils;
-
-import java.time.Duration;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
