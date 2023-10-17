01b0056d15d9d8682f7c96f14fca3f42b6011f3a
Yahor Labanau <yahorlabanau@gmail.com>
2023-10-17 13:56:20 +0300

    231017 add refactored GroupTestscriptCollaboratoriumTest class with tests by me

--- src/test/java/school/redrover/GroupTestscriptCollaboratoriumTest.java
+++ src/test/java/school/redrover/GroupTestscriptCollaboratoriumTest.java
@@ -8,20 +8,16 @@ import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
 
-import static org.testng.Assert.assertEquals;
-
-
-public class GroupTestscriptCollaboratoriumTest extends BaseTest{
+public class GroupTestscriptCollaboratoriumTest extends BaseTest {
     @Ignore
     @Test
-    public void getGuru() throws InterruptedException {
+    public void testGetGuru() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.guru99.com/");
 
         String title = driver.getTitle();
-        assertEquals("Meet Guru99 – Free Training Tutorials & Video for IT Courses", title);
+        Assert.assertEquals("Meet Guru99 – Free Training Tutorials & Video for IT Courses", title);
 
 
         WebElement JUnitButton = driver.findElement(By.xpath("//*[@data-lasso-id='147439']"));
@@ -30,64 +26,85 @@ public class GroupTestscriptCollaboratoriumTest extends BaseTest{
         Thread.sleep(900);
 
         WebElement textButton = driver.findElement(By.xpath("//*[@id='post-862']/div/div/h2[2]"));
-        Assert.assertEquals(textButton.getText(),"JUnit Tutorial Syllabus");
+        Assert.assertEquals(textButton.getText(), "JUnit Tutorial Syllabus");
 
         driver.quit();
     }
+
+    @Ignore
     @Test
-    public void testSubscription(){
+    public void testSubscription() {
 
-            getDriver().get("https://murzilka.org/");
+        WebDriver driver = new ChromeDriver();
+        try {
 
-            String title = getDriver().getTitle();
+            driver.get("https://murzilka.org/");
+
+            String title = driver.getTitle();
             Assert.assertEquals(title, "Журнал \"Мурзилка\"");
 
-            WebElement textButton = getDriver().findElement(By.xpath("//*[@class='mrb-btn-item-text']"));
+            WebElement textButton = driver.findElement(By.xpath("//*[@class=\"mrb-btn-item-text\"]"));
             String valueButton = textButton.getText();
             Assert.assertEquals(valueButton, "Подписаться на журнал");
 
 
             textButton.click();
-            WebElement message = getDriver().findElement(By.xpath("//h1[@class='category-name']"));
+            WebElement message = driver.findElement(By.xpath("//h1[@class=\"category-name\"]"));
             String valueH1 = message.getText();
             Assert.assertEquals(valueH1, "РЕДАКЦИОННАЯ ПОДПИСКА");
 
+        } finally {
+            driver.quit();
+        }
     }
+
+    @Ignore
     @Test
-    public void testAddToBasket() throws InterruptedException{
+    public void testAddToBasket() {
 
-            getDriver().get("https://murzilka.org/products/category/redaktsionnaya-podpiska");
-            WebElement addButton = getDriver().findElement(By.xpath("//button[@class='button product-item__button button_for_product-card cart-btn js-order-product js-cart-btn']"));
+        WebDriver driver = new ChromeDriver();
+        try {
+
+            driver.get("https://murzilka.org/products/category/redaktsionnaya-podpiska");
+            WebElement addButton = driver.findElement(By.xpath("//button[@class=\"button product-item__button button_for_product-card cart-btn js-order-product js-cart-btn\"]"));
             addButton.click();
-            Thread.sleep(200);
 
-            WebElement inBasket = getDriver().findElement(By.xpath("//*[@class='quantity-items top-cart__quantity']"));
+            WebElement inBasket = driver.findElement(By.xpath("//*[@class=\"quantity-items top-cart__quantity\"]"));
             String valueBasket = inBasket.getText();
             Assert.assertEquals(valueBasket, "1");
 
+        } finally {
+            driver.quit();
+        }
     }
+
     @Test
-    public void testSearch(){
-        JenkinsUtils.login(getDriver());
-        Assert.assertEquals(
-                getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText(),
-                "Welcome to Jenkins!");
+    public void testEssayAppGetSite() {
+
+        getDriver().get("https://essay.app/");
+
+        String title = getDriver().getTitle();
+        Assert.assertEquals(title, "Essay - Write better.");
     }
+
     @Test
-    public void testVersion(){
+    public void testEssayAppSwitchWebsiteColorLayout() {
 
-        JenkinsUtils.login(getDriver());
+        testEssayAppGetSite();
 
-        WebElement buttonVersion = getDriver().findElement(By.xpath("//*[@id='jenkins']/footer/div/div[2]/button"));
-        buttonVersion.click();
+        WebElement upperSitePanel = getDriver().findElement(By.className("css-s5xdrg"));
 
-        WebElement buttonVersionNext = getDriver().findElement(By.xpath("//a[@href='/manage/about']"));
-        buttonVersionNext.click();
+        WebElement colorLayoutSwitcher = upperSitePanel.findElement(By.tagName("button"));
+        String colorLayoutSwitcherCurrentState = colorLayoutSwitcher.getAttribute("aria-checked");
 
-        WebElement version = getDriver().findElement(By.xpath("//p[@class='app-about-version']"));
-        String valueVersion = version.getText();
-        Assert.assertEquals(valueVersion, "Version 2.414.2");
+        Assert.assertEquals(colorLayoutSwitcherCurrentState, "true",
+                "Current layout is Dark");
 
+        WebElement colorLayoutSwitcherHandle = colorLayoutSwitcher.findElement(By.className("ant-switch-handle"));
+        colorLayoutSwitcherHandle.click();
 
+        colorLayoutSwitcherCurrentState = colorLayoutSwitcher.getAttribute("aria-checked");
+        Assert.assertEquals(colorLayoutSwitcherCurrentState, "false",
+                "Current layout is Light");
     }
 }
