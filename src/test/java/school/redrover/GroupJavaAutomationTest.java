d3ed43006796734bf7768e92fc7b8cbc29bc3995
davidAffman <affmandavid@gmail.com>
2023-10-18 06:01:26 +0300

    delete old test

--- src/test/java/school/redrover/GroupJavaAutomationTest.java
+++ src/test/java/school/redrover/GroupJavaAutomationTest.java
@@ -1,25 +1,9 @@
 package school.redrover;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
-import java.io.File;
-import java.net.URI;
-import java.time.Duration;
-import org.testng.asserts.SoftAssert;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-import static org.testng.Assert.assertEquals;
 
 
 public class GroupJavaAutomationTest extends BaseTest {
