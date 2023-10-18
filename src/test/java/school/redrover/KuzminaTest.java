81c32599ccfd266967807c403f51648cce8b98a8
Svetlana <svetlanalearn@mail.ru>
2023-10-18 16:50:40 -0400

    first jenkins test: added description

--- src/test/java/school/redrover/KuzminaTest.java
+++ src/test/java/school/redrover/KuzminaTest.java
@@ -10,20 +10,19 @@ import school.redrover.runner.BaseTest;
 public class KuzminaTest extends BaseTest {
 
     @Test
-    public void testJenkins() {
+    public void testAddedDescription() {
+
+        String myInputText = "My description\nSecond Line";
 
         getDriver().findElement(By.id("description-link")).click();
 
         WebElement textAreaDescription = getDriver().findElement(By.className("jenkins-input"));
-
-        textAreaDescription.sendKeys("My description\nSecond Line");
-
+        textAreaDescription.sendKeys(myInputText);
         getDriver().findElement(By.name("Submit")).click();
 
         String descriptionText = getDriver().findElement(By.xpath("//*[@id=\"description\"]/div[1]")).getText();
 
-        Assert.assertEquals(descriptionText, "My description\nSecond Line");
-
+        Assert.assertEquals(descriptionText, myInputText);
 
     }
 }
