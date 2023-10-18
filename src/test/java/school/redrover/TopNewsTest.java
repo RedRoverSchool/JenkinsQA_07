6b883dee1a4a7bfa316f40aa42144ef86e57a2f3
Omegakot <v.a.evtushenko@yandex.ru>
2023-10-18 22:17:20 +0300

    update tests 4 Jenkins

--- src/test/java/school/redrover/TopNewsTest.java
+++ src/test/java/school/redrover/TopNewsTest.java
@@ -3,19 +3,15 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-import school.redrover.runner.JenkinsUtils;
-import school.redrover.runner.ProjectUtils;
+import org.openqa.selenium.Keys;
 
 
-@Ignore
+
+
 public class TopNewsTest extends BaseTest {
-
-
-
+    private static final String SEARCH = "ASDASDASD";
 
     @Test(description = "Проверка Заголовка приветствия")
     public void testJenkinsAuthorization() {
@@ -29,20 +25,44 @@ public class TopNewsTest extends BaseTest {
     public void testCheckUrl() {
 
         getDriver().findElement(By.xpath("//span[contains(text(),'Create a job')]")).click();
+
         String actualURL = getDriver().getCurrentUrl();
 
         Assert.assertEquals(actualURL, "http://localhost:8080/newJob", "URL не совпадает");
     }
 
     @Test
-    public void testName() {
+    public void testContextLinks() {
+
+        getDriver().findElement(By.xpath("//a[@href = '/me/my-views']")).click();
+        String expectedTextResult = getDriver().findElement(By.xpath("//h2[@class ='h4']")).getText();
+
+        getDriver().findElement(By.xpath("//a[@href = '/me/']")).click();
+        String expectedTextResultLoginName = getDriver().findElement(By.xpath("//div[contains(text(),'Jenkins')]"))
+                .getText().substring(16).trim();
+        String actualTextResultLoginName = getDriver().findElement(By.xpath("//div[@id= 'main-panel']/h1")).getText().trim();
+
+        Assert.assertEquals(expectedTextResult, "This folder is empty", " context не совпадает");
+        Assert.assertEquals(expectedTextResultLoginName, actualTextResultLoginName, " context не совпадает");
     }
 
     @Test
-    public void testName() {
+    public void testSearchContext() {
+
+        getDriver().findElement(By.xpath("//input[@id= 'search-box']")).sendKeys(SEARCH);
+        getDriver().findElement(By.xpath("//input[@id= 'search-box']")).sendKeys(Keys.ENTER);
+
+        String expectedTextResultSearch = getDriver().findElement(By.className("error")).getText();
+
+        Assert.assertEquals(expectedTextResultSearch, "Ничего похожего не найдено", " context не совпадает");
+
     }
 
     @Test
-    public void testName() {
+    public void testCheckUrlHelpJenkins() {
+        getDriver().findElement(By.xpath("//input[@id= 'search-box']")).click();
+        getDriver().findElement(By.className("main-search__icon-trailing")).click();
+
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://www.jenkins.io/doc/book/using/searchbox/", "URL не совпадает");
     }
 }
