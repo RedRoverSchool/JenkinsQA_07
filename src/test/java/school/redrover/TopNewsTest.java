23949ef985cf7a29f55e23effae5b300762d166c
Omegakot <v.a.evtushenko@yandex.ru>
2023-10-18 20:27:56 +0300

    update tests 4 Jenkins

--- src/test/java/school/redrover/TopNewsTest.java
+++ src/test/java/school/redrover/TopNewsTest.java
@@ -14,34 +14,9 @@ import school.redrover.runner.ProjectUtils;
 @Ignore
 public class TopNewsTest extends BaseTest {
 
-    private static final String BASEURL = "https://topnews.ru";
-@Ignore
-    @Test(description = "Сравнение контента заголовков в первом фрейме и в первом блоке боковой панели ")
-    public void testContent1() {
-        getDriver().get(BASEURL);
-        String mainTitle = getDriver().findElement(By.xpath("//div[@class = 'first-news-title']")).getText();
-        int sub = mainTitle.indexOf("(");
-        mainTitle = mainTitle.substring(0, sub);
-        getDriver().findElement(By.xpath("//div[@class = 'first-news-title']")).click();
-        String sideTitle = getDriver().findElement(By.xpath(" //div[@class ='top-news-item'][1]")).getText();
-        sideTitle = sideTitle.substring(0, sub);
-        Assert.assertEquals(mainTitle, sideTitle, "Заголовки не совпадают");
-    }
 
 
-    @Test(description = "Сравнение главного заголовка в разделе о проекте ")
-    public void testContent2() {
-        String TITLE_ABOUT = "TOPNEWS — этот рейтинг делаешь только ты!";
-        getDriver().get(BASEURL);
-        getDriver().findElement(By.id("menu-item-73200")).click();
-        getDriver().findElement(By.id("menu-item-73201")).click();
-        String getTitleAboutProject = getDriver().findElement(By.className("about-title")).getText();
 
-        Assert.assertEquals(getTitleAboutProject, TITLE_ABOUT, "Заголовки не совпадают");
-
-    }
-
-@Ignore
     @Test(description = "Проверка Заголовка приветствия")
     public void testJenkinsAuthorization() {
 
@@ -49,9 +24,9 @@ public class TopNewsTest extends BaseTest {
 
         Assert.assertEquals(actualInfo, "Start building your software project", "Заголовок не совпадает");
     }
-@Ignore
+
     @Test(description = "Проверка адреса URL страницы новой Job")
-    public void testJenkins() {
+    public void testCheckUrl() {
 
         getDriver().findElement(By.xpath("//span[contains(text(),'Create a job')]")).click();
         String actualURL = getDriver().getCurrentUrl();
@@ -59,4 +34,15 @@ public class TopNewsTest extends BaseTest {
         Assert.assertEquals(actualURL, "http://localhost:8080/newJob", "URL не совпадает");
     }
 
+    @Test
+    public void testName() {
+    }
+
+    @Test
+    public void testName() {
+    }
+
+    @Test
+    public void testName() {
+    }
 }
