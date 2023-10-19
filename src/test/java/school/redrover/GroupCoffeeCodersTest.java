877b5cfc363d75fbe7c468d48804df0aa97cdf4f
Daniil-Kurochkin <kurochkin-daniil98@mail.ru>
2023-10-19 23:52:28 +0300

    Creat new project and new folder

--- src/test/java/school/redrover/GroupCoffeeCodersTest.java
+++ src/test/java/school/redrover/GroupCoffeeCodersTest.java
@@ -3,12 +3,11 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 
-public class GroupCoffeeCoders extends BaseTest {
+public class GroupCoffeeCodersTest extends BaseTest {
 
     @Test
     public void testProject1() {
