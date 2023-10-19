2059fba227c6b2fd6c3cbd66d69b32f1c690e343
ArturSabanadze <Diakosmisi26@hotmail.com>
2023-10-19 22:40:25 +0200

    1.Delete old tests 2.Add a Test for Jenkins

--- src/test/java/school/redrover/ArSaFirstTest.java
+++ src/test/java/school/redrover/ArSaFirstTest.java
@@ -1,45 +1,41 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class ArSaFirstTest extends BaseTest {
 
+    private final static String folderName = "Artur Sabanadze";
+    private final static String expectedVersion = "Jenkins 2.414.2";
+
     @Test
     public void testCreateFolder() {
 
-        // Set folder name
-        final String folderName = "Artur Sabanadze";
+        final String actualVersion = getDriver().findElement(By.className("jenkins_ver")).getText();
 
-        //Navigate to the new job creation page
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
 
-        //Enter folder name in the input field
         getDriver().findElement(By.id("name")).sendKeys(folderName);
 
-        //Select the folder type (Organization Folder in this case)
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
 
-        //Click the "OK" button to create the folder
         getDriver().findElement(By.id("ok-button")).click();
 
-        //Click the "Save" button to save the folder
         getDriver().findElement(By.name("Submit")).click();
 
-        //Navigate to the created folder
-        getDriver().findElement(By.className("model-link")).click();
+        getDriver().findElement(By.linkText("Dashboard")).click();
         getDriver().findElement(By.cssSelector("li.jenkins-breadcrumbs__list-item a.model-link")).click();
         getDriver().findElement(By.linkText(folderName)).click();
 
-        //Navigate to the configuration page of the folder
         getDriver().findElement(By.linkText("Configure")).click();
-
-        //Enter description in the textarea
         getDriver().findElement(By.name("_.description")).sendKeys("Organization File of Artur Sabanadze. Student of Redrover School (7)");
-
-        //Click the "Save" button to submit the changes
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        getDriver().findElement(By.linkText("Dashboard")).click();
+
+        Assert.assertNotNull(getDriver().findElement(By.id("job_Artur Sabanadze")), "Artur Sabanadze folder not found on the Jenkins home page");
+        Assert.assertEquals(actualVersion, expectedVersion, "Jenkins version mismatch");
 
     }
 }
