622ee683b68b75f566994dc3585665ba3b7154dd
AkikoAoki <iakikoaokii@gmail.com>
2023-10-21 22:47:20 +0400

    add new test

--- src/test/java/school/redrover/PlusThreeTest.java
+++ src/test/java/school/redrover/PlusThreeTest.java
@@ -13,7 +13,6 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
 
 import java.time.Duration;
 import java.util.List;
@@ -33,6 +32,22 @@ public class PlusThreeTest extends BaseTest {
     public static final String CURRENT_ADDRESS = "USA";
     //---------------------------------------------------------------------------------------------------------------------------------
     private static final String SAUCEDEMO_URL = "https://www.saucedemo.com/";
+
+    @Test (description = "Test creating new item")
+    public void testNewItem () {
+
+    final String itemName = "Akiko";
+    getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+    getDriver().findElement(By.id("name")).sendKeys(itemName);
+    getDriver().findElement(By.xpath("//li[@class = 'hudson_model_FreeStyleProject']")).click();
+    getDriver().findElement(By.id("ok-button")).click();
+    getDriver().findElement(By.name("Submit")).click();
+    
+
+    Assert.assertNotNull(getDriver().findElement(By.xpath("//a[@href = '/job/" + itemName + "/']")), "Ссылка на страницу созданного Item не была найдена");
+    Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + itemName);
+
+    }
 
     public static class DataProviders {
 
