c8a326abf26e50256120eabe63c849cb287c0d06
Halyna A <7108581@gmail.com>
2023-10-20 19:06:14 -0700

    refactor

--- src/test/java/school/redrover/AfanasiukTest.java
+++ src/test/java/school/redrover/AfanasiukTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -13,11 +12,7 @@ public class AfanasiukTest extends BaseTest {
     public void testCreateFreeStyleProject() {
 
         getDriver().findElement(By.xpath("//a [@href='newJob']")).click();
-
-        WebElement nameInput = getDriver().findElement(By.xpath("//input [@name='name']"));
-        nameInput.click();
-        nameInput.sendKeys("Freestyle Project");
-
+        getDriver().findElement(By.xpath("//input [@name='name']")).sendKeys("Freestyle Project");
         getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
         getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
@@ -31,12 +26,8 @@ public class AfanasiukTest extends BaseTest {
     public void testCreatePipeline() {
 
         getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]/span/a/span[1]")).click();
-
-        WebElement nameInput = getDriver().findElement(By.xpath("//input [@name='name']"));
-        nameInput.click();
-        nameInput.sendKeys("Pipeline");
-
-        getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']")).click();
+        getDriver().findElement(By.xpath("//input [@name='name']")).sendKeys("Pipeline");
+                getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']")).click();
         getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
