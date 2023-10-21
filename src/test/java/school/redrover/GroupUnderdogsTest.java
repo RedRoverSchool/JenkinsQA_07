511ad4cb431f0a01dbd85c9999920c2ea9c65809
Olgla <elenagladenko@gmail.com>
2023-10-21 11:14:14 -0400

    Verify New Freestyle project created on a Dashboard with UUID random name

--- src/test/java/school/redrover/GroupUnderdogsTest.java
+++ src/test/java/school/redrover/GroupUnderdogsTest.java
@@ -4,11 +4,39 @@ import org.openqa.selenium.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.util.UUID;
+
 import static org.testng.Assert.assertEquals;
 
 public class GroupUnderdogsTest extends BaseTest {
 
     private static final String STR_TEST = "test";
+    private static final String DASHBOARD_PAGE= "//a[text()='Dashboard']";
+
+    @Test
+    public void testNewProjectCreatedOlena() {
+        String randomName = UUID.randomUUID()
+                .toString()
+                .substring(0, 5);
+        WebElement newItem = getDriver().findElement(By.linkText("New Item"));
+        newItem.click();
+
+        WebElement projectNameField = getDriver().findElement(By.id("name"));
+        projectNameField.click();
+        projectNameField.sendKeys(randomName);
+
+        WebElement selectProjectType = getDriver().findElement(By.xpath("//span[text()='Freestyle project']"));
+        selectProjectType.click();
+
+        WebElement okButton = getDriver().findElement(By.id("ok-button"));
+        okButton.click();
+
+        getDriver().findElement(By.xpath(DASHBOARD_PAGE)).click();
+        WebElement projectName = getDriver().findElement(By.xpath("//td[3]/a"));
+        String actualProjectName = projectName.getText();
+        assertEquals(randomName, actualProjectName);
+    }
 
     @Test
     public void testJenkinsVersionInFooter_tereshenkov29() {
