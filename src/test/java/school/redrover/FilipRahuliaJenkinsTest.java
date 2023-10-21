a7be5a72f8d4cac333aca3ef1d1d9c8c0ef5acd4
FilipR <fififima92@gmail.com>
2023-10-21 12:13:29 +0200

    add first test for Jenkins removed old tests

--- src/test/java/school/redrover/FilipRahuliaJenkinsTest.java
+++ src/test/java/school/redrover/FilipRahuliaJenkinsTest.java
@@ -7,16 +7,14 @@ import school.redrover.runner.BaseTest;
 
 public class FilipRahuliaJenkinsTest extends BaseTest {
 
+    private static final String testDescriptionText = "Test description";
+
     @Test
     public void testEditDescription() {
-        String testDescriptionText = "Test description";
 
         getDriver().findElement(By.id("description-link")).click();
-
         getDriver().findElement(By.name("description")).sendKeys(testDescriptionText);
-
         getDriver().findElement(By.name("Submit")).click();
-
         String actualDescriptionText = getDriver().findElement(By.xpath
                 ("//div[@id='description']/div[1]")).getText();
 
