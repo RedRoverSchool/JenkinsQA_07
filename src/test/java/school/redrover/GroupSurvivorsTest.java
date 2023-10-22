078bda11849394ec7b15b51d5ea3579b8c3aacaf
Lelik-gal <147049745+Lelik-gal@users.noreply.github.com>
2023-10-22 18:53:48 +0300

    Add a new test

--- src/test/java/school/redrover/GroupSurvivorsTest.java
+++ src/test/java/school/redrover/GroupSurvivorsTest.java
@@ -1,28 +1,21 @@
 package school.redrover;
-
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-
 public class GroupSurvivorsTest extends BaseTest {
-
     @Test
-    public void testEvgenyCheckJenkinsVersion() {
-        getDriver().findElement(By.cssSelector(".jenkins-button.jenkins-button--tertiary.jenkins_ver")).click();
-        getDriver().findElement(By.xpath("//*[@class = 'jenkins-dropdown__item'][1]")).click();
+    public void testOlgaTransitionAnalysis() {
 
-        Assert.assertEquals(getDriver().findElement(By.className("app-about-version")).getText(), "Version 2.414.2");
-    }
+        getDriver().findElement(
+                By.xpath(("//*[@id='jenkins']/footer/div/div[2]/button"))).click();
+        getDriver().findElement(
+                By.className(("jenkins-dropdown__item"))).click();
+        getDriver().findElement(
+                By.xpath(("//*[@id='jenkins']/footer/div/div[2]/button"))).click();
 
-    @Test
-    public void testEvgenyAddDescription() {
-        getDriver().findElement(By.id("description-link")).click();
-        getDriver().findElement(By.xpath("//*[@name = 'description']")).sendKeys("Test description");
-        getDriver().findElement(By.xpath("//*[@id = 'description']/form/div[2]/button")).click();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id = 'description']/div")).getText(), "Test description");
+        Assert.assertEquals(getDriver().getTitle(), "О Jenkins 2.414.3 [Jenkins]");
     }
 
 }
