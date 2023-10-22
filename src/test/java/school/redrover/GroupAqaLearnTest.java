ae3b8b8cb70302c8ad828e345fda07baee39d043
Nikolai Markow <i@nikomar.ru>
2023-10-22 20:36:51 +0700

    add CreateFreestyleProject

--- src/test/java/school/redrover/GroupAqaLearnTest.java
+++ src/test/java/school/redrover/GroupAqaLearnTest.java
@@ -1,20 +1,115 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.WheelInput;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import school.redrover.runner.JenkinsUtils;
-
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class GroupAqaLearnTest extends BaseTest {
+    private WebDriverWait wait2;
+    private WebDriverWait wait5;
+    private WebDriverWait wait10;
+    private static final String NAME_PROJECT = "Hello world";
+
+    protected WebDriverWait getWait5() {
+        if (wait5 == null) {
+            wait5 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        }
+        return wait5;
+    }
+
+    protected WebDriverWait getWait2() {
+        if (wait2 == null) {
+            wait2 = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        }
+        return wait2;
+    }
+
+    protected WebDriverWait getWait10() {
+        if (wait10 == null) {
+            wait10 = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        }
+        return wait10;
+    }
+
+
+    public void scroll(int deltaY) {
+        new Actions(getDriver())
+                .scrollFromOrigin(WheelInput.ScrollOrigin.fromViewport(), 0, deltaY)
+                .perform();
+    }
+
+    public void clickPerform(By by) {
+        new Actions(getDriver())
+                .click(getDriver().findElement(by))
+                .perform();
+    }
+
+    public void clickPerform(WebElement webElement) {
+        new Actions(getDriver())
+                .click(webElement)
+                .perform();
+    }
+
+    public void CreateFreestyleProjectJob(String nameProject) {
+        getDriver().findElement(
+                By.xpath("//*[@id='tasks']/div[1]/span/a")
+        ).click();
+        getDriver().findElement(
+                By.name("name")
+        ).sendKeys(NAME_PROJECT);
+        getDriver().findElement(
+                By.cssSelector(".icon-freestyle-project")
+        ).click();
+
+        clickPerform(By.xpath("//*[@id='ok-button']"));
+        getDriver().findElement(
+                By.xpath("//*[@name='description']")
+        ).sendKeys(NAME_PROJECT.concat(" java test program"));
+        scroll(600);
+
+        clickPerform(getWait2().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@for='radio-block-1']"))));
+        getWait2().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@checkdependson='credentialsId']")
+        )).sendKeys("https://github.com/kriru/firstJava.git");
+        scroll(2000);
+
+        getWait2().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id='yui-gen9-button']")
+        )).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id=\"yui-gen16\"]")
+        )).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@name='description']")
+        )).sendKeys("javac ".concat(NAME_PROJECT.concat(".java\njava ".concat(NAME_PROJECT))));
+
+        getWait2().until(ExpectedConditions.elementToBeClickable(
+                By.name("Apply")
+        )).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(
+                By.name("Submit")
+        )).click();
+        clickPerform(getWait2().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id='tasks']/div[4]/span/a"))));
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@class='model-link inside build-link display-name']")
+        )).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@class='icon-terminal icon-xlg']")
+        )).click();
+    }
+
     @Test
     public void testFirstJenkins() {
 
@@ -24,28 +119,6 @@ public class GroupAqaLearnTest extends BaseTest {
                 getDriver().findElement(By.xpath("//*[@class='h3']")).getText(),
                 "Enter an item name");
 
-    }
-
-    @Ignore
-    @Test
-    public void testNameOfTitle() {
-
-        String url = "https://openweathermap.org/";
-        String expectedResult = "OpenWeather";
-
-        WebDriver driver = new ChromeDriver();
-
-        driver.get(url);
-
-        WebElement searchTitle = driver.findElement
-                (
-                        By.xpath("//div/h1")
-                );
-        String actualResult = searchTitle.getText();
-
-        Assert.assertEquals(actualResult, expectedResult);
-
-        driver.quit();
     }
 
     @Test
@@ -70,5 +143,14 @@ public class GroupAqaLearnTest extends BaseTest {
         }
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void CreateFreestyleProject() {
+        CreateFreestyleProjectJob("RedRover");
+
+        Assert.assertTrue(getWait5().until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@class='console-output']")
+        )).getText().contains("Finished: SUCCESS"));
     }
 }
