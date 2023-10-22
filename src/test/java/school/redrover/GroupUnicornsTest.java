f7f1983066e2cc6b9b9a58337103846ac41a4630
Sergey.D <sergei.demyanenko@gmail.com>
2023-10-21 21:26:31 -0400

    Test

--- src/test/java/school/redrover/GroupUnicornsTest.java
+++ src/test/java/school/redrover/GroupUnicornsTest.java
@@ -494,8 +494,7 @@ public class GroupUnicornsTest extends BaseTest {
         Thread.sleep(1000);
 
         new Actions(getDriver())
-                .moveToElement(getDriver().findElement(By.xpath("//*[contains(@data-href, '/job/Bayans_job/')]")))
-                .click()
+                .click(getDriver().findElement(By.xpath("//*[contains(@data-href, '/job/Bayans_job/')]")))
                 .perform();
 
         Thread.sleep(1000);
