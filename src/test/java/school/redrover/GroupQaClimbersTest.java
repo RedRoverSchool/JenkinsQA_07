abbbbb77b1cfd73effec2c5f001a34dd85df4d9c
YuliaKonkova <johndoe@example.com>
2023-10-23 12:23:46 +0300

    test testDashboardList refactored

--- src/test/java/school/redrover/GroupQaClimbersTest.java
+++ src/test/java/school/redrover/GroupQaClimbersTest.java
@@ -1,8 +1,6 @@
 package school.redrover;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
