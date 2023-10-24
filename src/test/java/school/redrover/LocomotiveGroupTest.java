aea085d79a2c72044cdaa47d3d399d4460948c72
Elisey93 <eliseyrodriguez@gmail.com>
2023-10-24 17:27:28 +0100

    refactor: remove all non-Jenkins tests

--- src/test/java/school/redrover/LocomotiveGroupTest.java
+++ src/test/java/school/redrover/LocomotiveGroupTest.java
@@ -11,73 +11,6 @@ import school.redrover.runner.BaseTest;
 
 public class LocomotiveGroupTest extends BaseTest {
 
-      @Test
-    public void testYandexSearchBar(){
-        String url = "https://ya.ru/";
-        getDriver().get(url);
-        try {
-
-            WebElement searchBar = getDriver().findElement(By.xpath("//div[@class='search3__input-wrapper']/input"));
-            WebElement searchButton = getDriver().findElement(By.xpath("//button[@class='search3__button mini-suggest__button']"));
-            searchBar.click();
-            searchBar.sendKeys("Ответ на главный вопрос жизни");
-            searchButton.click();
-            WebElement searchText = getDriver().findElement(By.xpath("//div[text()='Ответ на главный вопрос жизни, вселенной и всего такого']"));
-            Assert.assertTrue(searchText.isDisplayed());
-        }catch (NoSuchElementException e){
-            System.out.println("Капча яндекса не позволяет закончить тест");
-        }finally {
-            System.out.println("Тест окончен");
-        }
-      }
-
-    @Ignore
-    @Test
-    public void testSimpleSearch() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("https://999.md/");
-
-        WebElement categoryButton = driver.findElement(By.xpath("//button[@id='js-categories-toggle']"));
-        categoryButton.click();
-
-        WebElement autoButton = driver.findElement(By.xpath("//a[contains(text(),'Autoturisme')]"));
-        autoButton.click();
-
-        Thread.sleep(1000);
-
-        WebElement masini = driver.findElement(By.xpath("//h1[contains(text(),'Mașini')]"));
-        String value = masini.getText();
-        Assert.assertEquals(value, "Mașini");
-
-        WebElement searchInput = driver.findElement(By.xpath("//input[@id='js-search-input']"));
-        searchInput.click();
-
-        WebElement input = driver.findElement(By.xpath("//input[@id='js-search-input']"));
-        input.sendKeys("audi a6");
-
-        WebElement searchButton = driver.findElement(By.xpath("//span[contains(.,'Caută')]"));
-        searchButton.click();
-
-        WebElement title = driver.findElement(By.xpath("//span[contains(text(),'Rezultatele căutării')]"));
-        String value1 = title.getText();
-        Assert.assertEquals(value1,"Rezultatele căutării \"audi a6\" (2 607 anunţuri)");
-
-        WebElement searchInput1 = driver.findElement(By.xpath("//input[@id='js-search-input']"));
-        searchInput1.click();
-
-        Thread.sleep(3000);
-
-        WebElement dropDown= driver.findElement(By.xpath("//html[1]/body[1]/div[3]/header[1]/div[2]/div[1]/form[1]/fieldset[1]/span[1]/div[1]/div[1]/div[1]"));
-        dropDown.click();
-
-        WebElement title1 = driver.findElement(By.xpath("//span[contains(text(),'Autoturisme')]"));
-        String value2 = title1.getText();
-        Assert.assertEquals(value2,"Autoturisme (600)");
-
-        driver.quit();
-    }
-
     @Test
     public void testVerifyJenkinsVersion() {
         WebDriver driver = getDriver();
@@ -97,17 +30,6 @@ public class LocomotiveGroupTest extends BaseTest {
         Assert.assertEquals(driver.findElement(locatorTextJenkinsVersion).getText(),
                 expectedJenkinsVersionText,
                 "Jenkins Version is incorrect");
-    }
-
-    @Test
-    public void testSkateSiteHeader() throws InterruptedException {
-        getDriver().get("http://www.skate.net/");
-
-        WebElement title = getDriver().findElement(By.className("header_text"));
-        String value = title.getText();
-        Assert.assertEquals(value, "skates, skate gear, ice skates and more");
-
-        Thread.sleep(2000);
     }
 
     @Test
