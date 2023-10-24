2886554eb308753c8b6af06da1fa9a757533be12
YuliaKonkova <johndoe@example.com>
2023-10-24 12:29:58 +0300

    tests testNewJobIsSuccessfullyBuilt, testProjectsNameOnMainPage added

--- src/test/java/school/redrover/GroupQaClimbersTest.java
+++ src/test/java/school/redrover/GroupQaClimbersTest.java
@@ -1,6 +1,5 @@
 package school.redrover;
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
