f554912d95798a58b9f4b54bf2fa7e6c6ba5cc22
Paviel Gorlitsyn <paviel.gorlitsyn@gmail.com>
2023-10-24 20:07:20 -0400

    Updated the tests from non Jenkins to Jenkins related.

--- src/test/java/school/redrover/GorlitsynTest.java
+++ src/test/java/school/redrover/GorlitsynTest.java
@@ -1,35 +1,51 @@
 package school.redrover;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import java.time.Duration;
 
-@Ignore
 public class GorlitsynTest extends BaseTest {
 
+    private WebDriverWait getWait2() {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+    }
 
-    @Test
-    public void testPetStoreTitle(){
+    private WebDriverWait getWait5() {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+    }
 
-
-        getDriver().get("https://petstore.octoperf.com/");
-        String title = getDriver().getTitle();
-        Assert.assertEquals(title, "JPetStore Demo");
-
+    public  void createFreestyleProject() {
+        getDriver().findElement(By.xpath("//a[@href= '/view/all/newJob']")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys("NewTestProject");
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        getDriver().findElement(By.name("Submit"));
+        getDriver().findElement(By.xpath("//img[@id='jenkins-head-icon']")).click();
     }
 
     @Test
-    public void testOpenPetStoreCatalog(){
-        String expectedFooter = "www.mybatis.org";
-        getDriver().get("https://petstore.octoperf.com/");
-        WebElement enterTheStoreButton = getDriver().findElement(By.linkText("Enter the Store"));
-        enterTheStoreButton.click();
-
-        String footer = getDriver().findElement(By.xpath("//a[@href = 'http://www.mybatis.org']")).getText();
-        Assert.assertEquals(footer,expectedFooter);
+    public void testFreestyleProjectCreation() {
+        createFreestyleProject();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//tr[@id=\"job_NewTestProject\"]")).getText(), "NewTestProject\n" +
+                "N/A N/A N/A");
     }
+    @Test
+    public void testFreestyleProjectDelete() {
+        createFreestyleProject();
+        getDriver().findElement(By.xpath("//a[@href='job/NewTestProject/']")).click();
+        getDriver().findElement(By.xpath("//a[@data-message=\"Delete the Project ‘NewTestProject’?\"]")).click();
+        getDriver().switchTo().alert().accept();
+        getDriver().findElements(By.xpath("e"));
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[text()='Welcome to Jenkins!']")).getText(), "Welcome to Jenkins!");
+
+
+    }
+
+
+
 
 }
 
