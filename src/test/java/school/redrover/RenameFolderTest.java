98030b991e11f12b43a148f405d5314a54445eae
Omegakot <v.a.evtushenko@yandex.ru>
2023-10-25 20:35:31 +0300

    US_04.001.01 add test Rename Folder

---
+++ src/test/java/school/redrover/RenameFolderTest.java
@@ -0,0 +1,42 @@
+package school.redrover;
+
+import school.redrover.runner.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import javax.swing.*;
+
+public class RenameFolderTest extends BaseTest {
+
+    final String nameFolder = "NewFolder";
+
+    private void createFolder() {
+
+        getDriver().findElement(By.xpath("//a[@href = 'newJob']")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(nameFolder);
+        getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
+        getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+
+        getDriver().findElement(By.xpath("//td/a[@href = 'job/" + nameFolder + "/']")).click();
+
+    }
+
+    @Test(description = "Переименование папки")
+    public void testRename() {
+        String renewNameFolder = "NewFolderTest";
+        createFolder();
+
+        getDriver().findElement(By.xpath("//a[@href='/job/" + nameFolder + "/confirm-rename']")).click();
+        getDriver().findElement(By.xpath("//input[@name='newName']")).clear();
+        getDriver().findElement(By.xpath("//input[@name='newName']")).sendKeys(renewNameFolder);
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+        String actualNameFolder = getDriver().findElement(By.xpath("//div[@id ='main-panel']/h1")).getText();
+
+        Assert.assertEquals(actualNameFolder, renewNameFolder, "Заголовок Папки не совпадает");
+    }
+
+}
