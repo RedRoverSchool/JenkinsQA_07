16ca3780087ab0d9224f888a10f547db34c2f34f
Astakhova <kseniorra@gmail.com>
2023-10-26 16:59:54 +0300

    TC_00.006.02 | refactor

--- src/test/java/school/redrover/MultibranchPipeline2Test.java
+++ src/test/java/school/redrover/MultibranchPipeline2Test.java
@@ -6,22 +6,20 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class MultibranchPipeline2Test extends BaseTest {
+
     @Test
-    public void testMultibranchNameDisplay() {
+    public void testMultibranchNameDisplayBreadcrumbTrail() {
+
+        final String pipelineName = "Multi";
 
         getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Multi");
-        getDriver().findElement(By.xpath("//span[@class='label' and text()='Multibranch Pipeline']")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(pipelineName);
+        getDriver().findElement(By.xpath("//span[contains(text(),'Multibranch Pipeline')]")).click();
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-        String pipelineName =  getDriver().findElement(By.xpath("//a[@href='/job/Multi/' and @class='model-link']")).getText();
-        Assert.assertEquals(pipelineName, "Multi");
-        String pipelineStatusName = getDriver().findElement(By.xpath("//h1")).getText();
-        Assert.assertEquals(pipelineStatusName, "Multi");
-        getDriver().findElement(By.xpath("//img[@alt='Jenkins']")).click();
-        String pipelineDashboardName = getDriver().findElement(By.xpath("//span[contains(text(), 'Multi')]")).getText();
-        Assert.assertEquals(pipelineDashboardName, "Multi");
-
+        String pipelineNameExpected =  getDriver().findElement(By.xpath("//a[contains(text(),'" + pipelineName + "')]")).getText();
+        Assert.assertEquals(pipelineNameExpected, pipelineName);
+        
     }
 
 
