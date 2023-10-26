6eedce42b5ef13eef7fc1a8ebb37fd0b94b80bcc
lelik <lelik_gal@mail.ru>
2023-10-26 19:06:12 +0300

    added test changes TC_01.002.02  Freestyle Project. Rename project

--- src/test/java/school/redrover/FreestyleProject2Test.java
+++ src/test/java/school/redrover/FreestyleProject2Test.java
@@ -5,6 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import static org.testng.Assert.assertEquals;
+
 public class FreestyleProject2Test extends BaseTest {
 
     @Test
@@ -24,5 +26,28 @@ public class FreestyleProject2Test extends BaseTest {
         Assert.assertEquals(
                 getDriver().findElement(By.cssSelector("#main-panel > h1")).getText(),
                 "Project " + projectName);
+    }
+
+    @Test
+    public void testGiveNewNameProject() {
+        String jobName = "freestyleJob";
+        String newJobName = "freestyleJob-2";
+
+        getDriver().findElement(By.className("task-icon-link")).click();
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(jobName);
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']/label[1]")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().navigate().back();
+        getDriver().navigate().back();
+
+        getDriver().findElement(By.xpath("//span[contains(text(),'" + jobName + "')]")).click();
+        getDriver().findElement(By.xpath("//div[@id='tasks']/div[7]/span")).click();
+        getDriver().findElement(By.xpath("//input[@value='" + jobName + "']")).clear();
+        getDriver().findElement(By.xpath("//input[@value='" + jobName + "']")).sendKeys(newJobName);
+        //getDriver().findElement(By.xpath("//div[@id='bottom-sticker']")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        assertEquals(getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[3]/a")).getText(),newJobName);
+
     }
 }
