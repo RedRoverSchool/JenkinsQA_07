f256ef856f59de2955d055066f366b35fe660540
Ksu007 <kazanchuk@ukr.net>
2023-10-26 17:44:09 +0300

    TC_06.003.02 | Organization folder > Rename Organization folder/ 6

--- src/test/java/school/redrover/OrganizationFolderRenameTest.java
+++ src/test/java/school/redrover/OrganizationFolderRenameTest.java
@@ -1,12 +1,8 @@
 package school.redrover;
-
-
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-
 
 public class OrganizationFolderRenameTest extends BaseTest {
 
@@ -20,6 +16,8 @@ public class OrganizationFolderRenameTest extends BaseTest {
         getDriver().findElement(By.xpath("//*[@id=\"jenkins-name-icon\"]")).click();
         getDriver().findElement(By.cssSelector("td a[href='job/" + folderName + "/'] span")).click();
 
+        Assert.assertTrue(getDriver()
+                .findElement(By.xpath("//tr[@id='job_" + folderName + "']")).isDisplayed());
 
     }
     @Test
@@ -27,7 +25,7 @@ public class OrganizationFolderRenameTest extends BaseTest {
         final String folderName = "Test";
         createOrgFolder();
         final String newFolderName =  "New name";
-        getDriver().findElement(By.xpath("//td/a[@href = 'job/" + folderName + "/']")).click();
+        getDriver().findElement(By.xpath("//*[@id=\"job_" + folderName + "\"]/td[3]/a")).click();
         getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[8]/span/a")).click();
         getDriver().findElement(By.name("newName")).clear();
         getDriver().findElement(By.name("newName")).sendKeys(newFolderName);
@@ -37,9 +35,6 @@ public class OrganizationFolderRenameTest extends BaseTest {
                 getDriver().findElement(By.cssSelector("#main-panel > h1")).getText(),
                 newFolderName
         );
-
-
-
     }
 
 }
