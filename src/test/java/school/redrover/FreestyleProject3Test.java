2e66b2e9465f02f473fbd186200f8dc48aad3ada
KaterynaYazici <katerynayazici7@gmail.com>
2023-10-26 22:40:51 +0300

    TC_00.001 | creat FreestyleProject3 test

---
+++ src/test/java/school/redrover/FreestyleProject3Test.java
@@ -0,0 +1,28 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FreestyleProject3Test extends BaseTest {
+    @Test
+    public void testNewItemFreestyleProject() {
+        final String pipelineName = "New Item Freestyle";
+
+        getDriver().findElement(By.linkText("New Item")).click();
+
+        getDriver().findElement(By.cssSelector("input#name.jenkins-input")).sendKeys(pipelineName);
+        getDriver().findElement(By.xpath("//span[contains(text(),'Freestyle project')]")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+
+        getDriver().findElement(By.name("description")).sendKeys(pipelineName + "description");
+        getDriver().findElement(By.name("Submit")).click();
+
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[contains(text(),'" + pipelineName + "')]")).isDisplayed(),
+                "Everyrhing is working,dont forget to improve yout test");
+    }
+}
