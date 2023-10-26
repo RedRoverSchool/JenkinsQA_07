d4594279c003208a203a39e3ff21f8994b697b36
bzhumanova <bayan.zhumanova@gmail.com>
2023-10-26 17:49:00 -0400

    TC_04.001.07 | Folder > Rename Folder - Check error unsafe character TC_04.001.08 | Folder > Rename Folder - Check error for an ending period

--- src/test/java/school/redrover/FolderBayanTest.java
+++ src/test/java/school/redrover/FolderBayanTest.java
@@ -7,11 +7,13 @@ import school.redrover.runner.BaseTest;
 
 public class FolderBayanTest extends BaseTest
 {
+    private final String FOLDER_NAME = "FOLDER_NAME";
+    private final String RENAMED_FOLDER_NAME = "RENAMED_FOLDER_NAME";
 
-    private void create(String folderName)
+    private void create()
     {
         getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.id("name")).sendKeys(folderName);
+        getDriver().findElement(By.id("name")).sendKeys(FOLDER_NAME);
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
         getDriver().findElement(By.id("ok-button")).click();
     }
@@ -22,42 +24,59 @@ public class FolderBayanTest extends BaseTest
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
     }
 
-    private void goToRenamePage(String folder) {
+    private void goToRenamePage() {
         getDriver().findElement(By.id("jenkins-home-link")).click();
-        getDriver().findElement(By.xpath("//td/a[contains(.,'" + folder + "')]")).click();
-        getDriver().findElement(By.xpath("//a[@href='/job/" + folder + "/confirm-rename']")).click();
+        getDriver().findElement(By.xpath("//td/a[contains(.,'" + FOLDER_NAME + "')]")).click();
+        getDriver().findElement(By.xpath("//a[@href='/job/" + FOLDER_NAME + "/confirm-rename']")).click();
     }
 
     @Test
     //https://trello.com/c/AZlAR8HB/109-tc0400104-us04001-folder-rename-folder
     public void testRenameThroughSidePanel()
     {
-        final String folderName = "FOLDER";
-        final String renamedFolderName = "RENAMED_FOLDER";
-
-        create(folderName);
-
-        goToRenamePage(folderName);
-
-        rename(renamedFolderName);
+        create();
+        goToRenamePage();
+        rename(RENAMED_FOLDER_NAME);
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1[contains(.,'RENAMED_FOLDER')]")).getText(),
-                renamedFolderName, "Renamed folder name is not matching to the expected renamed name " + renamedFolderName);
+                RENAMED_FOLDER_NAME, "Renamed folder name is not matching to the expected renamed name " + RENAMED_FOLDER_NAME);
     }
 
     @Test
     //https://trello.com/c/HpLFOFj4/112-tc0400105-folder-rename-folder-check-error-when-no-name-is-specified
     public void testRenameErrorNoNameSpecified() {
-        final String folderName = "FOLDER";
-
-        create(folderName);
-
-        goToRenamePage(folderName);
-
+        create();
+        goToRenamePage();
         rename("");
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//p")).getText(),
                 "No name is specified");
 
+    }
+
+    @Test
+    //https://trello.com/c/qKuYXV9W/180-tc0400107-folder-rename-folder-check-error-unsafe-character
+    public void testRenameWithUnsafeCharacter() {
+        char unsafeCharacter = '*'; // $ # / : ? [ \
+
+        create();
+        goToRenamePage();
+        rename("FOLDER_WITH_UNSAFE_CHARACTER_"+unsafeCharacter);
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//p")).getText(),
+                "‘" + unsafeCharacter +"’ is an unsafe character");
+    }
+
+    @Test
+    //https://trello.com/c/2lI25hTY/184-tc0400108-folder-rename-folder-check-error-for-an-ending-period
+    public void testRenameWithEndingPeriod() {
+        char period = '.';
+
+        create();
+        goToRenamePage();
+        rename("FOLDER_WITH_UNSAFE_CHARACTER"+period);
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//p")).getText(),
+                "A name cannot end with ‘" + period + "’");
     }
 }
