1b544f2c17e4af7aed2b60ad2227d3bb03a24fea
bzhumanova <bayan.zhumanova@gmail.com>
2023-10-25 22:46:10 -0400

    TC_04.001.05 | Folder > Rename Folder - Check error when no name is specified

--- src/test/java/school/redrover/FolderBayanTest.java
+++ src/test/java/school/redrover/FolderBayanTest.java
@@ -10,7 +10,7 @@ public class FolderBayanTest extends BaseTest
 
     private void create(String folderName)
     {
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.linkText("New Item")).click();
         getDriver().findElement(By.id("name")).sendKeys(folderName);
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
         getDriver().findElement(By.id("ok-button")).click();
@@ -22,21 +22,42 @@ public class FolderBayanTest extends BaseTest
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
     }
 
+    private void goToRenamePage(String folder) {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+        getDriver().findElement(By.xpath("//td/a[contains(.,'" + folder + "')]")).click();
+        getDriver().findElement(By.xpath("//a[@href='/job/" + folder + "/confirm-rename']")).click();
+    }
+
     @Test
     //https://trello.com/c/AZlAR8HB/109-tc0400104-us04001-folder-rename-folder
-    public void renameThroughSidePanel()
+    public void testRenameThroughSidePanel()
     {
+        final String folderName = "FOLDER";
         final String renamedFolderName = "RENAMED_FOLDER";
 
-        create("FOLDER");
+        create(folderName);
 
-        getDriver().findElement(By.id("jenkins-home-link")).click();
-        getDriver().findElement(By.xpath("//td/a[contains(.,'FOLDER')]")).click();
-        getDriver().findElement(By.xpath("//a[@href='/job/FOLDER/confirm-rename']")).click();
+        goToRenamePage(folderName);
 
         rename(renamedFolderName);
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1[contains(.,'RENAMED_FOLDER')]")).getText(),
                 renamedFolderName, "Renamed folder name is not matching to the expected renamed name " + renamedFolderName);
+    }
+
+    @Test
+    //https://trello.com/c/HpLFOFj4/112-tc0400105-folder-rename-folder-check-error-when-no-name-is-specified
+    public void testRenameErrorNoNameSpecified() {
+        final String folderName = "FOLDER";
+
+        create(folderName);
+
+        goToRenamePage(folderName);
+
+        rename("");
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//p")).getText(),
+                "No name is specified");
+
     }
 }
