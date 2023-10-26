22ac3703bba8dcb636cc64c4626917ceeed26df3
sergeykazak <sergey.kazakov@gmail.com>
2023-10-27 00:22:49 +0300

    TC_00.001.04 | Added Create Freestyle project

---
+++ src/test/java/school/redrover/FreestyleProject4Test.java
@@ -0,0 +1,29 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+@Test
+public class FreestyleProject4Test extends BaseTest {
+
+
+    @Test
+    public void testFreestyleProject() {
+        final String projectName = "FreestyleProject";
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys("FreestyleProject");
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
+        getDriver().findElement(By.xpath("//table//a[@href='job/FreestyleProject/']")).click();
+        WebElement disableButton = getDriver().findElement(By.xpath("//button[@name='Submit']"));
+        boolean disableButtonIsDisplayed = disableButton.isDisplayed();
+        Assert.assertTrue(disableButtonIsDisplayed,"We are now on Configuration screen");
+    }
+}
