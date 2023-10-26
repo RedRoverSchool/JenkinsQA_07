c64d864c2f87fdd22b87f47ba46fc5b6d65500a9
Omegakot <v.a.evtushenko@yandex.ru>
2023-10-26 09:18:16 +0300

    fix test

--- src/test/java/school/redrover/Folder3Test.java
+++ src/test/java/school/redrover/Folder3Test.java
@@ -6,33 +6,34 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Folder3Test extends BaseTest {
-    final String nameFolder = "NewFolder";
+    private final String NAMEFOLDER = "NewFolder";
 
-    private void createFolder() {
+    private final String RENAMEFOLDER = "NewFolderTest";
+
+    private void createFolder(String newName) {
 
         getDriver().findElement(By.xpath("//a[@href = 'newJob']")).click();
-        getDriver().findElement(By.className("jenkins-input")).sendKeys(nameFolder);
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(newName);
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
         getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
         getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
         getDriver().findElement(By.id("jenkins-name-icon")).click();
-        getDriver().findElement(By.xpath("//td/a[@href = 'job/" + nameFolder + "/']")).click();
+        getDriver().findElement(By.xpath("//td/a[@href = 'job/" + newName + "/']")).click();
 
     }
 
     @Test(description = "Rename Folder")
     public void testRename() {
-        String renewNameFolder = "NewFolderTest";
 
-        createFolder();
+        createFolder(NAMEFOLDER);
 
-        getDriver().findElement(By.xpath("//a[@href='/job/" + nameFolder + "/confirm-rename']")).click();
+        getDriver().findElement(By.xpath("//a[@href='/job/" + NAMEFOLDER + "/confirm-rename']")).click();
         getDriver().findElement(By.xpath("//input[@name='newName']")).clear();
-        getDriver().findElement(By.xpath("//input[@name='newName']")).sendKeys(renewNameFolder);
+        getDriver().findElement(By.xpath("//input[@name='newName']")).sendKeys(RENAMEFOLDER);
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         String actualNameFolder = getDriver().findElement(By.xpath("//div[@id ='main-panel']/h1")).getText();
 
-        Assert.assertEquals(actualNameFolder, renewNameFolder, "Заголовок переименованной не совпадает");
+        Assert.assertEquals(actualNameFolder, RENAMEFOLDER, "Заголовок переименованной не совпадает");
     }
 
 }
