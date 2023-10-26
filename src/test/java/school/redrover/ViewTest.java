4440635fdbdef0071190ee6bc61fc3f421cf55bb
Denys <taranchik68@gmail.com>
2023-10-27 00:20:30 +0300

    TC_10.001.02 |  Create new view

--- src/test/java/school/redrover/ViewTest.java
+++ src/test/java/school/redrover/ViewTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -28,6 +29,29 @@ public class ViewTest extends BaseTest {
         getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).submit();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='/user/admin/my-views/view/Test%20view/']")).getText(), nameView);
+    }
+
+    @Test
+    public void testCreateNewView2() {
+
+        final String MyProjectName = "My new freestyle project name";
+        final String NewViewName = "My new view name";
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@name = 'name']")).sendKeys(MyProjectName);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
+
+        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+
+        getDriver().findElement(By.xpath("//a[@tooltip = 'New View']")).click();
+
+        getDriver().findElement(By.id("name")).sendKeys(NewViewName);
+        getDriver().findElement(By.xpath("//label[@for='hudson.model.MyView']")).click();
+        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id=\"projectstatus-tabBar\"]/div/div[1]/div[2]/a")).getText(),  NewViewName);
     }
 
 }
