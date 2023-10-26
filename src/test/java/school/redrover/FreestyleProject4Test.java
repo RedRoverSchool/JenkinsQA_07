d21dff6a4844fecf0abd4c79fa0b4a64482d6884
KaterynaYazici <katerynayazici7@gmail.com>
2023-10-26 23:26:11 +0300

    TC_00.001.08 | create FreestyleProject4 test_solve conflict

--- src/test/java/school/redrover/FreestyleProject4Test.java
+++ src/test/java/school/redrover/FreestyleProject4Test.java
@@ -19,8 +19,7 @@ public class FreestyleProject4Test extends BaseTest {
         getDriver().findElement(By.id("jenkins-home-link")).click();
         getDriver().findElement(By.xpath("//tr[@id= 'job_" + projectName + "' ] //td[3]/a")).click();
 
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//h1")).getText(),
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(),
                 "Project " + projectName);
     }
 }
