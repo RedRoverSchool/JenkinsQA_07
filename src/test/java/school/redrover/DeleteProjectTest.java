ab425dac4e3da1c3ef4362cc0ef455921242f146
Omegakot <v.a.evtushenko@yandex.ru>
2023-10-26 20:53:58 +0300

    TC_01.003 add test delete project

---
+++ src/test/java/school/redrover/DeleteProjectTest.java
@@ -0,0 +1,36 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class DeleteProjectTest extends BaseTest {
+
+    private static final String NAMEFOLDER = "MyFolder";
+
+
+    public void createProject(String name) {
+
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(name);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
+
+        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+
+    }
+
+    @Test
+    public void testProject() {
+        createProject("MyFolder");
+        getDriver().findElement(By.xpath("//a[contains(@class, 'confirmation-link')]")).click();
+        getDriver().switchTo().alert().accept();
+
+        String actualTitle = getDriver().findElement(By.xpath("//h2[contains(text(),'Start building your software project')]")).getText();
+
+        Assert.assertEquals(actualTitle, "Start building your software project");
+
+    }
+}
