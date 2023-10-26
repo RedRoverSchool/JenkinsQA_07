5a7751a19043a121f995ec42b6563e68e8091faa
Ksu007 <kazanchuk@ukr.net>
2023-10-26 17:47:29 +0300

    TC_06.003.02 | Organization folder > Rename Organization folder/ 7

--- src/test/java/school/redrover/OrganizationFolderRenameTest.java
+++ src/test/java/school/redrover/OrganizationFolderRenameTest.java
@@ -16,8 +16,7 @@ public class OrganizationFolderRenameTest extends BaseTest {
         getDriver().findElement(By.xpath("//*[@id=\"jenkins-name-icon\"]")).click();
         getDriver().findElement(By.cssSelector("td a[href='job/" + folderName + "/'] span")).click();
 
-        Assert.assertTrue(getDriver()
-                .findElement(By.xpath("//tr[@id='job_" + folderName + "']")).isDisplayed());
+
 
     }
     @Test
