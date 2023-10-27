535b846749b937a36f3d83a2eb33c45b638e7e41
Railia Borobova <digulyonka@gmail.com>
2023-10-27 12:13:31 -0700

    TC_12.004.03 | Added test for footer rest API link and checked 3 API option types

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -1,9 +1,15 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.collections.Sets;
 import school.redrover.runner.BaseTest;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class FooterTest extends BaseTest {
 
@@ -33,4 +39,20 @@ public class FooterTest extends BaseTest {
 
         Assert.assertTrue(getDriver().getCurrentUrl().contains("api"));
     }
+
+    @Test
+    public void testRestApiPageOpensAndHas3ApiOptions() {
+        clickRestApi();
+
+        List<WebElement> apiTypes = getDriver().findElements(By.xpath("//div[@id='main-panel']/dl/dt/a"));
+        Assert.assertEquals(apiTypes.size(), 3, "REST API page should always have 3 API types");
+
+        Set<String> apiTypeText = new HashSet<>();
+        for (WebElement el : apiTypes) {
+            apiTypeText.add(el.getText());
+        }
+        Set<String> expected = Sets.newHashSet("XML API", "JSON API", "Python API");
+        Assert.assertEquals(apiTypeText, expected);
+    }
+
 }
