bdbbf4d9c6993a7b19e44d0daaf9d100b3d72c6d
bzhumanova <bayan.zhumanova@gmail.com>
2023-10-26 21:57:06 -0400

    TC_04.002.03 | Folder > Move Folder to Folder through the side panel

--- src/test/java/school/redrover/FolderBayanTest.java
+++ src/test/java/school/redrover/FolderBayanTest.java
@@ -1,19 +1,24 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FolderBayanTest extends BaseTest
 {
-    private final String FOLDER_NAME = "FOLDER_NAME";
-    private final String RENAMED_FOLDER_NAME = "RENAMED_FOLDER_NAME";
+    private final String MAIN_FOLDER_NAME = "Main_folder_name";
+    private final String NESTED_FOLDER_NAME = "Nested_folder_name";
+    private final String RENAMED_FOLDER_NAME = "Renamed_folder_name";
 
-    private void create()
+    private void create(String nameFolder)
     {
         getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.id("name")).sendKeys(FOLDER_NAME);
+        getDriver().findElement(By.id("name")).sendKeys(nameFolder);
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
         getDriver().findElement(By.id("ok-button")).click();
     }
@@ -24,29 +29,38 @@ public class FolderBayanTest extends BaseTest
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
     }
 
-    private void goToRenamePage() {
+    private void goTheFolderPage(String folderName) {
+        getDriver().findElement(By.xpath("//a[@href='job/" + folderName + "/']")).click();
+    }
+
+    private void goToDashboard() {
         getDriver().findElement(By.id("jenkins-home-link")).click();
-        getDriver().findElement(By.xpath("//td/a[contains(.,'" + FOLDER_NAME + "')]")).click();
-        getDriver().findElement(By.xpath("//a[@href='/job/" + FOLDER_NAME + "/confirm-rename']")).click();
     }
 
     @Test
     //https://trello.com/c/AZlAR8HB/109-tc0400104-us04001-folder-rename-folder
     public void testRenameThroughSidePanel()
     {
-        create();
-        goToRenamePage();
+        create(MAIN_FOLDER_NAME);
+        goToDashboard();
+
+        goTheFolderPage(MAIN_FOLDER_NAME);
+        getDriver().findElement(By.xpath("//a[@href='/job/" + MAIN_FOLDER_NAME + "/confirm-rename']")).click();
+
         rename(RENAMED_FOLDER_NAME);
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[contains(.,'RENAMED_FOLDER')]")).getText(),
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1[contains(.,'" + RENAMED_FOLDER_NAME + "')]")).getText(),
                 RENAMED_FOLDER_NAME, "Renamed folder name is not matching to the expected renamed name " + RENAMED_FOLDER_NAME);
     }
 
     @Test
     //https://trello.com/c/HpLFOFj4/112-tc0400105-folder-rename-folder-check-error-when-no-name-is-specified
     public void testRenameErrorNoNameSpecified() {
-        create();
-        goToRenamePage();
+        create(MAIN_FOLDER_NAME);
+        goToDashboard();
+        goTheFolderPage(MAIN_FOLDER_NAME);
+        getDriver().findElement(By.xpath("//a[@href='/job/" + MAIN_FOLDER_NAME + "/confirm-rename']")).click();
+
         rename("");
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//p")).getText(),
@@ -59,8 +73,11 @@ public class FolderBayanTest extends BaseTest
     public void testRenameWithUnsafeCharacter() {
         char unsafeCharacter = '*'; // $ # / : ? [ \
 
-        create();
-        goToRenamePage();
+        create(MAIN_FOLDER_NAME);
+        goToDashboard();
+        goTheFolderPage(MAIN_FOLDER_NAME);
+        getDriver().findElement(By.xpath("//a[@href='/job/" + MAIN_FOLDER_NAME + "/confirm-rename']")).click();
+
         rename("FOLDER_WITH_UNSAFE_CHARACTER_"+unsafeCharacter);
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//p")).getText(),
@@ -72,11 +89,49 @@ public class FolderBayanTest extends BaseTest
     public void testRenameWithEndingPeriod() {
         char period = '.';
 
-        create();
-        goToRenamePage();
-        rename("FOLDER_WITH_UNSAFE_CHARACTER"+period);
+        create(MAIN_FOLDER_NAME);
+        goToDashboard();
+        goTheFolderPage(MAIN_FOLDER_NAME);
+        getDriver().findElement(By.xpath("//a[@href='/job/" + MAIN_FOLDER_NAME + "/confirm-rename']")).click();
+
+        rename("FOLDER_WITH_UNSAFE_CHARACTER" + period);
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//p")).getText(),
                 "A name cannot end with ‘" + period + "’");
+    }
+
+    @Test
+    //https://trello.com/c/Iq7pi6KS/186-tc0400203-folder-move-folder-to-folder-through-the-side-panel
+    public void testMoveThroughSidePanel() throws InterruptedException
+    {
+        create(MAIN_FOLDER_NAME);
+        goToDashboard();
+
+        create(NESTED_FOLDER_NAME);
+        goToDashboard();
+
+        goTheFolderPage(NESTED_FOLDER_NAME);
+        getDriver().findElement(By.linkText("Move")).click();
+        getDriver().findElement(By.name("destination")).click();
+        getDriver().findElement(By .xpath("//option[contains(text(),'Jenkins » " + MAIN_FOLDER_NAME + "')]")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+        goToDashboard();
+        goTheFolderPage(MAIN_FOLDER_NAME);
+        goTheFolderPage(NESTED_FOLDER_NAME);
+
+        ArrayList<String> expectedBreadcrumbs = new ArrayList<>();
+        expectedBreadcrumbs.add("Dashboard");
+        expectedBreadcrumbs.add(MAIN_FOLDER_NAME);
+        expectedBreadcrumbs.add(NESTED_FOLDER_NAME);
+
+        ArrayList<String> actualBreadcrumbs = new ArrayList<>();
+        List<WebElement> bredcrumbs = getDriver().findElements(By.xpath("//li/a[@class='model-link']"));
+        for (WebElement eachBreadcrumb: bredcrumbs)
+        {
+            actualBreadcrumbs.add(eachBreadcrumb.getText());
+        }
+
+        Assert.assertEquals(actualBreadcrumbs, expectedBreadcrumbs, "Breadcrumbs don't match");
     }
 }
