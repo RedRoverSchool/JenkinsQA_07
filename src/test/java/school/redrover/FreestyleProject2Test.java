05be25bd45eed1e32e4f85fa4beca5ce027314ef
lelik <lelik_gal@mail.ru>
2023-10-28 21:22:40 +0300

    add test  TC_01.003.14  Freestyle Project. Delete Project

--- src/test/java/school/redrover/FreestyleProject2Test.java
+++ src/test/java/school/redrover/FreestyleProject2Test.java
@@ -40,4 +40,28 @@ public class FreestyleProject2Test extends BaseTest {
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1[@class='job-index-headline page-headline']"))
                 .getText(),"Project Online store");
     }
+
+   public void testCreateNewItem(String itemName) {
+
+        getDriver().findElement(By.className("task-icon-link")).click();
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(itemName);
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']/label[1]")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().navigate().back();
+        getDriver().navigate().back();
+    }
+
+    @Test
+    public void testDeleteFreestyleProject() {
+        final String itemName = "Test Project";
+
+        testCreateNewItem(itemName);
+
+        getDriver().findElement(By.xpath("//span[contains(text(),'" + itemName + "')]")).click();
+        getDriver().findElement(By.xpath(" //*[@id='tasks']/div[6]/span/a")).click();
+        getDriver().switchTo().alert().accept();
+
+        Assert.assertEquals(getDriver().findElements(
+                By.xpath("//*[@id='breadcrumbs']/li[3]/a")).size(), 0);
+    }
 }
