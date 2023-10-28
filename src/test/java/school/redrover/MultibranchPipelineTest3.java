268d6f91c8de2261e83ed0e8f9e4358dc2bec18a
AlexAQA2023 <skytalker89@gmail.com>
2023-10-28 19:17:51 +0200

    Create testCreateFreeStyleProject method

---
+++ src/test/java/school/redrover/MultibranchPipelineTest3.java
@@ -0,0 +1,31 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class MultibranchPipelineTest3 extends BaseTest {
+    @Test
+    public void testMultibranchPipelineCreation() {
+
+        final var expectedPipelineName = "AutoPipelineName";
+
+        WebElement newJobButton = getDriver().findElement(By.xpath("//a[@href='newJob']"));
+        newJobButton.click();
+        WebElement nameFieldInput = getDriver().findElement(By.xpath("//input[@id='name']"));
+        nameFieldInput.sendKeys(expectedPipelineName);
+        WebElement multiBranchPipelineButton = getDriver().findElement(By.xpath("//span[contains(text(),'Multibranch Pipeline')]"));
+        multiBranchPipelineButton.click();
+        WebElement submitButton = getDriver().findElement(By.xpath("//button[@type='submit']"));
+        submitButton.click();
+        WebElement submitPipelineButton = getDriver().findElement(By.xpath("//button[@name='Submit']"));
+        submitPipelineButton.click();
+        WebElement createdPipeLineName = getDriver().findElement(By.xpath("//a[contains(text(),'" + expectedPipelineName + "')]"));
+        String actualPipelineName = createdPipeLineName.getText();
+
+        Assert.assertEquals(actualPipelineName, expectedPipelineName);
+
+    }
+}
