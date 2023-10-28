23e2798e288c44103c7dc8f566af6210f62e3998
DmitriyKlv <rtjoyhome8@gmail.com>
2023-10-28 16:48:52 +0200

    TC_00.001.12 | Creating new item > Creating Freestyle project

--- src/test/java/school/redrover/FreestyleProject7Test.java
+++ src/test/java/school/redrover/FreestyleProject7Test.java
@@ -11,16 +11,15 @@ public class FreestyleProject7Test extends BaseTest {
   public void testCreate() {
       final String projectname = "FreestyleProject";
 
-      getDriver().findElement(By.xpath("//*[@id=\'tasks\']/div[1]/span/a")).click();
-      getDriver().findElement(By.xpath("//*[@id=\'name\']")).sendKeys(projectname);
-      getDriver().findElement(By.xpath("//*[@id=\'j-add-item-type-standalone-projects\']/ul/li[1]/label/span"))
+      getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]/span/a")).click();
+      getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys(projectname);
+      getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[1]/label/span"))
               .click();
-      getDriver().findElement(By.xpath("//*[@id=\'ok-button\']"))
+      getDriver().findElement(By.xpath("//*[@id='ok-button']"))
               .click();
 
-
-      getDriver().findElement(By.xpath("//*[@id=\'bottom-sticker\']/div/button[1]")).click();
-      String actualName = getDriver().findElement(By.xpath("//*[@id=\'main-panel\']/h1")).getText();
+      getDriver().findElement(By.xpath("//*[@id='bottom-sticker']/div/button[1]")).click();
+      String actualName = getDriver().findElement(By.xpath("//*[@id='main-panel']/h1")).getText();
 
       Assert.assertEquals( "Project FreestyleProject", actualName);
   }
