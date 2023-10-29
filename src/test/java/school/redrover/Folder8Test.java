37619793ba43b24f44f12a4cc622f9792718a6b8
NadinLi <liuliakhin@gmail.com>
2023-10-29 09:59:34 -0700

    Merge remote-tracking branch 'origin/AT_00.007' into AT_00.007

---
+++ src/test/java/school/redrover/Folder8Test.java
@@ -0,0 +1,28 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class Folder8Test extends BaseTest {
+    @Test
+    public void testCreateFolderWithValidName() {
+        final String folderName = "MyFolder";
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.cssSelector(".jenkins-input")).sendKeys(folderName);
+        getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
+        getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
+
+        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+        getDriver().findElement(By.cssSelector("#jenkins-name-icon")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//td/a[@href='job/" + folderName + "/']")).getText(),
+                folderName);
+
+    }
+}
+
+
