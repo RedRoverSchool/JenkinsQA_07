76742e911d15fdc9a5c92a73cbcdbc6365ea08e4
Yuliyash1106 <yuliyash1106@gmail.com>
2023-10-29 19:47:44 -0400

    US_00.002.08 | Add Creating Pipeline test

---
+++ src/test/java/school/redrover/Pipeline8Test.java
@@ -0,0 +1,25 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class Pipeline8Test extends BaseTest {
+    @Test
+    public void testCreateNewPipeline() {
+
+        final String newPipeLineName = "PipeLineProject1";
+
+        getDriver().findElement(By.xpath("//a[@href = 'newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@name = 'name']")).sendKeys(newPipeLineName);
+        getDriver().findElement(By.xpath("//li[@class = 'org_jenkinsci_plugins_workflow_job_WorkflowJob']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        getDriver().findElement(By.xpath("//a[contains(text(), 'Dashboard')]")).click();
+        getDriver().findElement(By.xpath("//span[text() = 'PipeLineProject1']")).click();
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//h1[contains(text() ,'Pipeline PipeLineProject1')]")).getText(),
+                "Pipeline PipeLineProject1");
+    }
+}
