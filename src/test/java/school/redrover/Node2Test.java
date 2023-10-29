f370e885e0ed70c962f3e30dc2a719adb7b9f062
n-stepanova <nstepanovaqa@gmail.com>
2023-10-29 19:01:43 +0300

    TC_15.00.07 - create node from jenkins main panel

--- src/test/java/school/redrover/Node2Test.java
+++ src/test/java/school/redrover/Node2Test.java
@@ -1,17 +1,9 @@
 package school.redrover;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-import java.time.Duration;
-import java.util.List;
 
 public class Node2Test extends BaseTest {
     @Test
