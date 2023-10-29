c84bca60feb6a08204ddc2a6883f4bec238c7474
DmitriyKlv <rtjoyhome8@gmail.com>
2023-10-29 10:03:57 +0100

    TC_00.001.12

---
+++ src/test/java/school/redrover/FreestyleProject1Test.java
@@ -0,0 +1,24 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FreestyleProject1Test extends BaseTest {
+    private final static String PROJECT_NAME = "FreestyleProject";
+    @Test
+    public void testCreateFreestyleProject(){
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']"))
+                .sendKeys(PROJECT_NAME);
+        getDriver().findElement(By.xpath("(//span[@class='label'])[1]")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+        String actualName = getDriver()
+                .findElement(By.xpath("//*[@id='job_FreestyleProject']/td[3]")).getText();
+        Assert.assertEquals(actualName,PROJECT_NAME);
+    }
+}
