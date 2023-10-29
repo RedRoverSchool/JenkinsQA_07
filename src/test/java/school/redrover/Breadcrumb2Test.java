583537ce4d6629e4829f56f862563483bcbd96b2
Elena-Shagdurova <shagdurovva.elena@gmail.com>
2023-10-29 13:14:23 -0400

    TC_11.002.03 /Breadcrumb /Return on Main Page From Any Page by click Dashboard

--- src/test/java/school/redrover/Breadcrumb2Test.java
+++ src/test/java/school/redrover/Breadcrumb2Test.java
@@ -1,37 +1,97 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-import java.util.List;
+import java.util.Random;
 
 public class Breadcrumb2Test extends BaseTest {
+    final String jobName = "Unicorns" + getRandomNumber();
 
-    private void clickOnBuildHistoryBtn() {
+    private static String getRandomNumber() {
 
-        String builtHistoryBtn = "Built History";
-        List<WebElement> options = getDriver().findElements(By.xpath("//div[@class = 'task ']"));
-        for (WebElement item : options) {
-            if (item.getText().equals(builtHistoryBtn)) {
-                item.click();
-            }
-        }
+        Random random = new Random();
+        return String.valueOf(100000000 + random.nextInt(900000000));
+    }
+
+    private void searchDashboardBtnAndClick() {
+
+        getDriver().findElement(By.xpath("//li/a[@class = 'model-link']")).click();
+    }
+
+    private void createNewFreestyleProject() {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/']")).isDisplayed();
+        getDriver().findElement(By.name("name")).sendKeys(jobName);
+        getDriver().findElement(By.xpath("//img[@class = 'icon-freestyle-project icon-xlg']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
     }
 
     @Test
-    public void testReturnOnMainPageByClickDashboardBtn() {
+    public void testReturnOnMainPageFromNewItem() {
 
-        clickOnBuildHistoryBtn();
+        createNewFreestyleProject();
+        searchDashboardBtnAndClick();
 
-        getDriver().findElement(By.xpath("//li/a[@class = 'model-link']")).click();
+        Assert.assertTrue(getDriver().findElement(By.id("tasks")).isDisplayed());
+    }
 
-        Assert.assertEquals(getDriver()
-                        .findElement(By.xpath("//h1"))
-                        .getText(),
-                "Welcome to Jenkins!");
+    @Test
+    public void testReturnOnMainPageFromPeople() {
+
+        createNewFreestyleProject();
+        searchDashboardBtnAndClick();
+
+        getDriver().findElement(By.xpath("//a[@href = '/asynchPeople/']")).click();
+        if (getDriver().findElement(By.xpath("//h1")).isDisplayed()) {
+            searchDashboardBtnAndClick();
+        }
+        Assert.assertTrue(getDriver().findElement(By.id("breadcrumbBar")).isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.id("tasks")).isDisplayed());
+    }
+
+    @Test
+    public void testReturnOnMainPageFromBuildHistory() {
+
+        createNewFreestyleProject();
+        searchDashboardBtnAndClick();
+
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/builds']")).click();
+        if (getDriver().findElement(By.xpath("//h1")).isDisplayed()) {
+            searchDashboardBtnAndClick();
+        }
+        Assert.assertTrue(getDriver().findElement(By.id("breadcrumbBar")).isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.id("tasks")).isDisplayed());
+    }
+
+    @Test
+    public void testReturnOnMainPageFromManageJenkins() {
+
+        createNewFreestyleProject();
+        searchDashboardBtnAndClick();
+
+        getDriver().findElement(By.xpath("//a[@href = '/manage']")).click();
+        if (getDriver().findElement(By.xpath("//h1")).isDisplayed()) {
+            searchDashboardBtnAndClick();
+        }
+        Assert.assertTrue(getDriver().findElement(By.id("breadcrumbBar")).isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.id("tasks")).isDisplayed());
+    }
+
+    @Test
+    public void testReturnOnMainPageFromMyViews() {
+
+        createNewFreestyleProject();
+        searchDashboardBtnAndClick();
+
+        getDriver().findElement(By.xpath("//a[@href = '/me/my-views']")).click();
+        searchDashboardBtnAndClick();
+
+        Assert.assertTrue(getDriver().findElement(By.id("breadcrumbBar")).isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.id("tasks")).isDisplayed());
     }
 }
-
