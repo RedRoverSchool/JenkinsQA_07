e12733496f670fbee3a919a0f34ceb4ea9381792
Marta Shepel <marta17shepel@gmail.com>
2023-10-30 21:53:14 +0100

    TC_00.003.001

--- src/test/java/school/redrover/MulticonfigurationProject2Test.java
+++ src/test/java/school/redrover/MulticonfigurationProject2Test.java
@@ -8,6 +8,8 @@ import school.redrover.runner.BaseTest;
 public class MulticonfigurationProject2Test extends BaseTest {
     @Test
     public void testCreate () {
+        final String ProjectName = "MulticonfigurationProject";
+
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("MulticonfigurationProject");
         getDriver().findElement(By.xpath("//li[@class='hudson_matrix_MatrixProject']")).click();
@@ -16,10 +18,10 @@ public class MulticonfigurationProject2Test extends BaseTest {
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
 
-        getDriver().findElement(By.xpath("//td/a[@href='job/MulticonfigurationProject/']")).click();
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + ProjectName + "/']")).click();
         Assert.assertEquals(
                 getDriver().findElement(By.cssSelector(".matrix-project-headline.page-headline")).getText(),
-                "Project MulticonfigurationProject");
+                "Project " + ProjectName);
 
 
 
