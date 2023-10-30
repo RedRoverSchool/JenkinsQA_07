09e53fb0ac206fa7c12afa6e765dce297c782a77
Bezverkhova <60390586+Bezverkhova@users.noreply.github.com>
2023-10-30 14:17:07 -0500

    TC_04.003.01 test create Job inside Folder

--- src/test/java/school/redrover/Folder4Test.java
+++ src/test/java/school/redrover/Folder4Test.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class Folder4Test extends BaseTest {
@@ -24,10 +25,31 @@ public class Folder4Test extends BaseTest {
 
         navigateToDashboard();
 
-        getDriver().findElement(By.xpath(String.format("//span[contains(text(),'%s')]",folder1Name))).click();
+        getDriver().findElement(By.xpath(String.format("//span[contains(text(),'%s')]", folder1Name))).click();
 
         String nestedFolders = getDriver().findElement(By.xpath("//table[@id='projectstatus']")).getText();
         assertTrue(nestedFolders.contains(folder2Name));
+    }
+
+    @Test
+    public void testCreateJobInsideFolder() {
+        final String folderName = "NewFolder";
+        final String jobName = "NewProject";
+
+        createFolder(folderName);
+
+        getDriver().findElement(By.xpath(String.format("//a[@href='/job/%s/newJob']",folderName))).click();
+
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(jobName);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.className("jenkins-button--primary")).click();
+
+        navigateToDashboard();
+        getDriver().findElement(By.xpath(String.format("//*[@id='job_%s']/td[3]/a",folderName))).click();
+
+        String jobNameInFolder = getDriver().findElement((By.xpath("//table[@id='projectstatus']//td[3]"))).getText();
+        assertEquals(jobNameInFolder, jobName);
     }
 
     private void createFolder(String folderName) {
