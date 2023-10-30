afa31a979cdadc0c20eda49203f2d5dec0b3ea9f
sasha <bogdanovalexander1@gmail.com>
2023-10-30 14:51:34 -0500

    Fix imports

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -80,4 +80,23 @@ public class FreestyleProjectTest extends BaseTest {
         Assert.assertTrue(newProject.isDisplayed(), "Project was not renamed");
     }
 
+    @Test
+    public void testCreate2() {
+
+        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
+        getDriver().findElement(By.cssSelector(".jenkins-input")).sendKeys("FreeStyleProjectName");
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
+
+        getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
+        getDriver().findElement(By.cssSelector("#jenkins-name-icon")).click();
+
+        getDriver().findElement(By.xpath("//td/a[@href = 'job/FreeStyleProjectName/']")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.cssSelector("#main-panel > h1")).getText(),
+                "Project FreeStyleProjectName");
+
+    }
+
 }
