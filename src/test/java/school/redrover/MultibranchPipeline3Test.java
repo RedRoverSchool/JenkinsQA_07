3f7b7ae2442c2c83edd38edc1deddaf7c75fa76b
AlexAQA2023 <skytalker89@gmail.com>
2023-10-30 09:36:25 +0100

    Refactor testCreateFreeStyleProject method

--- src/test/java/school/redrover/MultibranchPipeline3Test.java
+++ src/test/java/school/redrover/MultibranchPipeline3Test.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class MultibranchPipeline3Test extends BaseTest {
+
     @Test
     public void testMultibranchPipelineCreation() {
 
@@ -16,13 +17,13 @@ public class MultibranchPipeline3Test extends BaseTest {
         newJobButton.click();
         WebElement nameFieldInput = getDriver().findElement(By.xpath("//input[@id='name']"));
         nameFieldInput.sendKeys(expectedPipelineName);
-        WebElement multiBranchPipelineButton = getDriver().findElement(By.xpath("//span[contains(text(),'Multibranch Pipeline')]"));
+        WebElement multiBranchPipelineButton = getDriver().findElement(By.xpath("//li[contains(@class,'multibranch')]"));
         multiBranchPipelineButton.click();
         WebElement submitButton = getDriver().findElement(By.xpath("//button[@type='submit']"));
         submitButton.click();
         WebElement submitPipelineButton = getDriver().findElement(By.xpath("//button[@name='Submit']"));
         submitPipelineButton.click();
-        WebElement createdPipeLineName = getDriver().findElement(By.xpath("//a[contains(text(),'" + expectedPipelineName + "')]"));
+        WebElement createdPipeLineName = getDriver().findElement(By.xpath("//h1"));
         String actualPipelineName = createdPipeLineName.getText();
 
         Assert.assertEquals(actualPipelineName, expectedPipelineName);
