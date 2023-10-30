06297ae50d77627983daae24b209866e7f189e46
bzhumanova <bayan.zhumanova@gmail.com>
2023-10-30 19:34:18 -0400

    TC_12.001.09 | Footer > Jenkins version > Get Involved

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -1,11 +1,31 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.ArrayList;
+
 public class FooterTest extends BaseTest {
+
+    @Test
+    //TC_12.001.09 | Footer > Jenkins version > Get Involved
+    public void testGetInvolved() throws InterruptedException
+    {
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);",
+                getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")));
+
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")).click();
+
+        getDriver().findElement(By.xpath("//a[@href='https://www.jenkins.io/participate/']")).click();
+
+        ArrayList<String> tab = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(tab.get(1));
+
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://www.jenkins.io/participate/");
+    }
 
     private void clickRestApi() {
         getDriver().findElement(By.xpath("//a[@href='api/']")).click();
