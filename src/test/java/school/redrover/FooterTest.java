fcf0fa5369efdfe17787becf99b719ebd5986b26
bzhumanova <bayan.zhumanova@gmail.com>
2023-10-30 17:44:10 -0400

    TC_12.001.05 | Footer > Jenkins version > Viewing the Jenkins version

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -1,11 +1,21 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class FooterTest extends BaseTest {
+
+    @Test
+    //https://trello.com/c/taPPdMEU/400-tc1200105-footer-jenkins-version-viewing-the-jenkins-version
+    public void testJenkinsVersion() {
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);",
+                getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")));
+        Assert.assertEquals(getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")).getText(),
+                "Jenkins 2.414.2");
+    }
 
     private void clickRestApi() {
         getDriver().findElement(By.xpath("//a[@href='api/']")).click();
