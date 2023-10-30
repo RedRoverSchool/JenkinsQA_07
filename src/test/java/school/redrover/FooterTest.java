5296f080795dc0d7d7a0947ce47bfee8fc15372c
bzhumanova <bayan.zhumanova@gmail.com>
2023-10-30 18:03:46 -0400

    https://trello.com/c/OOkxIvHX/402-tc1200107-footer-jenkins-version-check-the-tippy-box

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -1,12 +1,35 @@
 package school.redrover;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FooterTest extends BaseTest {
 
+    @Test
+    //https://trello.com/c/OOkxIvHX/402-tc1200107-footer-jenkins-version-check-the-tippy-box
+    public void checkTippyBox() {
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);",
+                getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")));
+
+        List<String> expectedMenu = List.of("About Jenkins", "Get involved", "Website");
+        List<String> actualMenu = new ArrayList<>();
+
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")).click();
+        List<WebElement> tippyBoxElements = getDriver().findElements(By.xpath("//div[@class = 'tippy-content']/a"));
+        for (WebElement tippyContectMenu : tippyBoxElements) {
+            actualMenu.add(tippyContectMenu.getText());
+        }
+
+        Assert.assertEquals(actualMenu, expectedMenu, "Tippy box context menu doesn't macth");
+    }
     private void clickRestApi() {
         getDriver().findElement(By.xpath("//a[@href='api/']")).click();
     }
