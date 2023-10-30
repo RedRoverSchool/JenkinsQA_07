2373bdba6a56ead572e21e8909cedc836680b6d6
katya-test <katyasuhr@gmail.com>
2023-10-30 13:38:13 -0400

    TC 06.003 | Rename Organization folder from Dashboard dropdwon menu

--- src/test/java/school/redrover/OrganizationFolder3Test.java
+++ src/test/java/school/redrover/OrganizationFolder3Test.java
@@ -91,11 +91,12 @@ public class OrganizationFolder3Test extends BaseTest {
 
         WebElement dropDownButton = getDriver().findElement(By.xpath("//td/a/button[@class = 'jenkins-menu-dropdown-chevron']"));
         new Actions(getDriver()).moveToElement(dropDownButton).perform();
-        dropDownButton.sendKeys(Keys.RETURN);
+        dropDownButton.click();
+//        dropDownButton.sendKeys(Keys.RETURN);
 
         WebDriverWait getWait2 = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
         getWait2.until(ExpectedConditions.visibilityOfElementLocated(By.id("tippy-5")));
-        getDriver().findElement(By.xpath("//a[contains(@href,'confirm-rename')]")).click();
+        getDriver().findElement(By.xpath("//a[contains(@href,'/confirm-rename')]")).click();
         getDriver().findElement(By.name("newName")).clear();
         getDriver().findElement(By.name("newName")).sendKeys(NEW_PROJECT_NAME);
         getDriver().findElement(By.name("Submit")).click();
