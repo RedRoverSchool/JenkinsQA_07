8002138cb2aa885be02e6aff463323583985c09a
bzhumanova <bayan.zhumanova@gmail.com>
2023-10-30 19:43:03 -0400

    TC_12.001.10 | Footer > Jenkins version > Website

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -1,12 +1,30 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.util.ArrayList;
+
 public class FooterTest extends BaseTest {
 
+    @Test
+    //https://trello.com/c/y8Xzp86w/405-tc1200109-footer-jenkins-version-website
+    public void testWebsite() {
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);",
+                getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")));
+
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--tertiary jenkins_ver']")).click();
+
+        getDriver().findElement(By.xpath("//a[@href='https://www.jenkins.io/']")).click();
+
+        ArrayList<String> tab = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(tab.get(1));
+
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://www.jenkins.io/");
+    }
     private void clickRestApi() {
         getDriver().findElement(By.xpath("//a[@href='api/']")).click();
     }
