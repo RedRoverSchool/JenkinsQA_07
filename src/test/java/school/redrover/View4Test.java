556026042b5cfa17031c6760aa393b79f3f3f974
Bsplv-1 <bsplv.m@gmail.com>
2023-10-31 16:16:28 +0500

    Merge remote-tracking branch 'origin/bsplv.m/new-test' into bsplv.m/new-test TC_10.001.07 Views > Create new view

--- src/test/java/school/redrover/View4Test.java
+++ src/test/java/school/redrover/View4Test.java
@@ -33,13 +33,43 @@ public class View4Test extends BaseTest {
 
     @Test
     public void testCreateNewView() {
-     createNewFreestyleProject();
-     goToHomepage();
-     createNewListView();
-     goToHomepage();
+        createNewFreestyleProject();
+        goToHomepage();
+        createNewListView();
+        goToHomepage();
 
-     Assert.assertEquals(getDriver().findElement(
-             By.xpath("//div[@class='tab']/a[@href='/view/ListView-1/']")).getText(),
-             "ListView-1");
+        Assert.assertEquals(getDriver().findElement(
+                        By.xpath("//div[@class='tab']/a[@href='/view/ListView-1/']")).getText(),
+                "ListView-1");
+    }
+
+    private void createNewFreestyleProjectT1() {
+        final String jobNameT1 = "FreestyleProjectT1";
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(jobNameT1);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+    }
+
+    private void goToHomepageT1() {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+    }
+
+    private void createNewListViewT1() {
+        final String viewNameT1 = "ListViewT1";
+        goToHomepageT1();
+        getDriver().findElement(By.cssSelector(".addTab")).click();
+        getDriver().findElement(By.xpath("//div/input[@id='name']")).sendKeys(viewNameT1);
+        getDriver().findElement(By.xpath("//label[@for='hudson.model.ListView']")).click();
+        getDriver().findElement(By.name("Submit")).click();
+    }
+    @Test
+        public void testCreateNewViewT1() {
+        createNewFreestyleProjectT1();
+        goToHomepageT1();
+        createNewListViewT1();
+        goToHomepageT1();
+
+        Assert.assertEquals(getDriver().findElement(By.cssSelector("a[href|='/view/ListViewT1/']")).getText(),"ListViewT1");
     }
 }
