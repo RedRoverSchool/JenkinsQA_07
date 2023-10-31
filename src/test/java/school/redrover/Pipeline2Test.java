c6a79a8d091d4cd79a419affdae09c5680f86c94
“Artuom <artuomkudryashov@gmail.com>
2023-10-31 19:51:55 -0400

    TC_000.002.020 | Creating new item > Creating PipelineRefactor3

--- src/test/java/school/redrover/Pipeline2Test.java
+++ src/test/java/school/redrover/Pipeline2Test.java
@@ -147,7 +147,7 @@ public class Pipeline2Test extends BaseTest {
     }
 
     @Test
-    public void testCreatingPipline() {
+    public void testCreatingPipeline() {
         String pipeline = "ArtusomPipeline";
         getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//div[@id='add-item-panel']//input[@id='name']")).sendKeys(pipeline);
