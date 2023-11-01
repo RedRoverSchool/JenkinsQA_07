e81b5acf82ca27fc601bfb2b9d90fbb15d715d40
Marta Shepel <marta17shepel@gmail.com>
2023-11-01 23:23:26 +0100

    Resolve conflict

--- src/test/java/school/redrover/OrganizationFolder9Test.java
+++ src/test/java/school/redrover/OrganizationFolder9Test.java
@@ -47,21 +47,19 @@ public class OrganizationFolder9Test extends BaseTest {
     }
 
     @Test
-    public void testCreateNewFolder () {
-        final String NewFolderName = "NewFolder00900";
+    public void testCreateOrganizationFolderWithValidName () {
+        final String OrganizationFolderName = "NewOrganizationFolder55";
 
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(NewFolderName);
-        getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(OrganizationFolderName);
+        getDriver().findElement(By.xpath("//li[@class='jenkins_branch_OrganizationFolder']")).click();
         getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
 
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
 
-        getDriver().findElement(By.xpath("//tr[@id='job_" + NewFolderName + "']//td[3]")).click();
         Assert.assertEquals(
-                getDriver().findElement(By.xpath("//tr[@id='job_" + NewFolderName + "']//td[3]")).getText(),
-                NewFolderName);
+                getDriver().findElement(By.xpath("//tr[@id='job_" + OrganizationFolderName + "']//td[3]")).getText(),
+                OrganizationFolderName);
     }
 }
-
