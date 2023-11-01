71f126d6ef61b2adf257bc0805db73cdf93b35a1
SerhiiHaponiuk <gaponyuk1994@gmail.com>
2023-11-01 20:35:29 +0200

    US_00.001.17 | Creating new item > Creating Freestyle project > With valid characters

--- src/test/java/school/redrover/FreestyleProject17Test.java
+++ src/test/java/school/redrover/FreestyleProject17Test.java
@@ -11,22 +11,14 @@ public class FreestyleProject17Test extends BaseTest {
     public void testCreateFreestyleProject() {
         final String freestyleProject = "Freestyle Project";
 
-        //"New item" element
         getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
 
-        //Enter project name
         getDriver().findElement(By.xpath("//input[@name = 'name']")).sendKeys(freestyleProject);
-
-        //Choose "Freestyle project" type
         getDriver().findElement(By.xpath("//span[text() = 'Freestyle project']")).click();
-
-        //CLick on "OK" button
         getDriver().findElement(By.xpath("//button[@id = 'ok-button']")).click();
 
-        ///Click on "Save" button
         getDriver().findElement(By.xpath("//button[@name = 'Submit']")).click();
 
-        //Check that project was created
         String nameOfProject = getDriver().findElement(By.xpath("//h1")).getText();
         Assert.assertTrue(nameOfProject.contains(freestyleProject));
     }
