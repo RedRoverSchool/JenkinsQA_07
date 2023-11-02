c5885b7dadd9f09efc69fe192bc2a64cf66eb54a
Ksu007 <kazanchuk@ukr.net>
2023-11-02 14:47:28 +0200

    TC_04.003.03 | refactored

--- src/test/java/school/redrover/Folder13Test.java
+++ src/test/java/school/redrover/Folder13Test.java
@@ -6,19 +6,19 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class Folder13Test extends BaseTest {
-    private final String folderName = "TestFolder";
-    private final String freestyleProjectName = "TestProject";
-    private void createFolder(String folderName) {
+    private static final String FOLDER_NAME = "TestFolder";
+    private static final String FREESTYLE_PROJECT_NAME = "TestProject";
+    private void createFolder() {
         getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.id("name")).sendKeys(folderName);
+        getDriver().findElement(By.id("name")).sendKeys(Folder13Test.FOLDER_NAME);
         getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.xpath("//button[@name=\"Submit\"]")).click();
         returnToDashboard();
     }
 
-    private void createFreestyleProject(String freestyleProjectName) {
-        getDriver().findElement(By.id("name")).sendKeys(freestyleProjectName);
+    private void createFreestyleProject() {
+        getDriver().findElement(By.id("name")).sendKeys(Folder13Test.FREESTYLE_PROJECT_NAME);
         getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.xpath("//button[@name=\"Submit\"]")).click();
@@ -30,12 +30,12 @@ public class Folder13Test extends BaseTest {
 
     @Test
     public void createNewJobInsideFolder() {
-        createFolder(folderName);
-        getDriver().findElement(By.xpath("//span[text()='" + folderName  + "']")).click();
+        createFolder();
+        getDriver().findElement(By.xpath("//span[text()='" + FOLDER_NAME + "']")).click();
         getDriver().findElement(By.className("content-block__link")).click();
-        createFreestyleProject(freestyleProjectName);
+        createFreestyleProject();
         returnToDashboard();
-        getDriver().findElement(By.xpath("//span[text()='" + folderName  + "']")).click();
-        Assert.assertTrue(getDriver().findElement(By.id("job_" + freestyleProjectName + "")).isDisplayed());
+        getDriver().findElement(By.xpath("//span[text()='" + FOLDER_NAME + "']")).click();
+        Assert.assertTrue(getDriver().findElement(By.id("job_" + FREESTYLE_PROJECT_NAME + "")).isDisplayed());
     }
 }
