a3db2cb6f452d3735ecb02a0de32bf4480222eb3
Maria Dymsha <dymsha.maria@mail.ru>
2023-11-02 14:14:05 +0300

    TC_00.002 | Creating new item > new Folder

---
+++ src/test/java/school/redrover/FolderNewTest2.java
@@ -0,0 +1,22 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FolderNewTest2 extends BaseTest {
+
+    @Test
+    public void createFolder1(){
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input']")).sendKeys("HSE");
+        getDriver().findElement(By.xpath("//*[text()='Folder']")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
+        getDriver().findElement(By.xpath("//li//a[@href='/']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//td//a//*[text()='HSE']")).getText(),"HSE");
+    }
+}
