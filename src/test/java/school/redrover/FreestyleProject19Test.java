4888b528c66618900d0c018d2aa77e7418f4bda7
VitalyRezanov <getahur.rezanov@gmail.com>
2023-11-02 18:38:28 +0300

    Update test to create FreestyleProject

--- src/test/java/school/redrover/FreestyleProject19Test.java
+++ src/test/java/school/redrover/FreestyleProject19Test.java
@@ -15,21 +15,21 @@ public class FreestyleProject19Test extends BaseTest {
         final String projectName = "FreestyleProject19Test";
         final WebDriver driver = getDriver();
 
-        driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a")).click();
-        driver.findElement(By.xpath("//*[@id=\"name\"]")).sendKeys(projectName);
+        driver.findElement(By.xpath("//*[@id='tasks']/div[1]/span/a")).click();
+        driver.findElement(By.xpath("//*[@id='name']")).sendKeys(projectName);
         driver.findElement(By
-                .xpath("//*[@id=\"j-add-item-type-standalone-projects\"]/ul/li[1]"))
+                .xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[1]"))
                 .click();
-        driver.findElement(By.xpath("//*[@id=\"ok-button\"]")).click();
+        driver.findElement(By.xpath("//*[@id='ok-button']")).click();
         driver.findElement(By
-                        .xpath("//*[@id=\"main-panel\"]/form/div[1]/div[2]/div/div[2]/textarea"))
+                        .xpath("//*[@id='main-panel']/form/div[1]/div[2]/div/div[2]/textarea"))
                 .sendKeys(projectName + " Description");
-        driver.findElement(By.xpath("//*[@id=\"bottom-sticker\"]/div/button[1]")).click();
+        driver.findElement(By.xpath("//*[@id='bottom-sticker']/div/button[1]")).click();
 
-        driver.findElement(By.xpath("//*[@id=\"jenkins-name-icon\"]")).click();
+        driver.findElement(By.xpath("//*[@id='jenkins-name-icon']")).click();
         List<WebElement> listItems = driver
                 .findElements(By
-                        .xpath("//*[@class=\"jenkins-table__link model-link inside\"]"));
+                        .xpath("//*[@class='jenkins-table__link model-link inside']"));
 
         Assert.assertTrue(listItems.stream().anyMatch(item -> item.getText().compareTo(projectName) == 0));
     }
