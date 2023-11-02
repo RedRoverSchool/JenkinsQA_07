51d15b59e259067ff81e64b4c4ad0f94d3685de3
AllaBird <allavorobieva@yahoo.com>
2023-11-02 17:31:06 -0400

    TC_04.002.05 | Move Folder to Folder from drop-down menu

--- src/test/java/school/redrover/Folder3Test.java
+++ src/test/java/school/redrover/Folder3Test.java
@@ -14,8 +14,10 @@ public class Folder3Test extends BaseTest {
         getDriver().findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder")).click();
         getDriver().findElement(By.id("ok-button")).click();
 
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-        getDriver().findElement(By.id("jenkins-name-icon")).click();
+        returnToJenkinsDashboard();
+    }
+    private void returnToJenkinsDashboard() {
+        getDriver().findElement(By.xpath("//a[@id = 'jenkins-home-link']")).click();
     }
 
     @Test
@@ -41,10 +43,28 @@ public class Folder3Test extends BaseTest {
         getDriver().findElement(By.xpath("//input[@name='newName']")).clear();
         getDriver().findElement(By.xpath("//input[@name='newName']")).sendKeys(renamedFolder);
         getDriver().findElement(By.name("Submit")).click();
-
-        getDriver().findElement(By.id("jenkins-name-icon")).click();
+        returnToJenkinsDashboard();
 
         Assert.assertEquals(getDriver().findElement(
                 By.xpath("//a[@class='jenkins-table__link model-link inside']")).getText(), renamedFolder);
+    }
+
+    @Test
+    public void testMoveFolderToFolder() {
+        createFolder("Main");
+        createFolder("Nested");
+
+        getDriver().findElement(By.xpath("//td/a[@href='job/Nested/']")).click();
+        getDriver().findElement(By.xpath("//a[@href='/job/Nested/move']")).click();
+        getDriver().findElement(By.name("destination")).click();
+        getDriver().findElement(By.xpath("//option[@value='/Main']")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        returnToJenkinsDashboard();
+
+        getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).click();
+        getDriver().findElement(By.xpath("//*[@id='job_Main']/td[3]/a/span")).click();
+
+        Assert.assertEquals(getDriver().findElement(
+                By.xpath("//td/a[@class='jenkins-table__link model-link inside']")).getText(), "Nested");
     }
 }
