866e23b992309395baf6eebf3c303580ff784df3
AnzhelikaLa <aalashko@gmail.com>
2023-11-02 20:22:59 -0500

    TC_09.001.06| Manage Jenkins > Create User > The created user record contains a clickable User ID button

--- src/test/java/school/redrover/NewUser2Test.java
+++ src/test/java/school/redrover/NewUser2Test.java
@@ -42,7 +42,7 @@ public class NewUser2Test extends BaseTest {
     private void createUserSuccess() {
         goToUserCreateFormPage();
         List<WebElement> valueInputs = getDriver().findElements(
-            By.xpath("//*[@class = 'jenkins-input']"));
+                By.xpath("//*[@class = 'jenkins-input']"));
         for (int i = 0; i < valueInputs.size(); i++) {
             if (i == 0) {
                 valueInputs.get(i).sendKeys(TEST_INPUT);
@@ -51,6 +51,29 @@ public class NewUser2Test extends BaseTest {
             }
         }
         getDriver().findElement(By.name("Submit")).click();
+    }
+    @Test
+    public void testUserRecordContainUserIdButton() {
+        getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
+        getDriver().findElement(By.xpath("//dd[contains(text(),'Create')]")).click();
+        getDriver().findElement(By.xpath("//a[contains(text(),'Create')]")).click();
+
+        WebElement username = getDriver().findElement(By.name("username"));
+        username.sendKeys("userTest");
+        WebElement password = getDriver().findElement(By.name("password1"));
+        password.sendKeys("@userTest");
+        WebElement confirm_password = getDriver().findElement(By.name("password2"));
+        confirm_password.sendKeys("@userTest");
+        WebElement full_name = getDriver().findElement(By.name("fullname"));
+        full_name.sendKeys("fullUserTest");
+        WebElement email_address = getDriver().findElement(By.name("email"));
+        email_address.sendKeys("userTest@gmail.com");
+        getDriver().findElement((By.name("Submit"))).click();
+
+        WebElement UserIdButton = getDriver().findElement(By.xpath("//tbody/tr[2]/td[2]/a[1]"));
+
+        Assert.assertEquals(UserIdButton.getText(), "userTest");
+
     }
 }
 
