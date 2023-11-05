88ba6b90a911a6c95a9c62583aa239fd8133561c
Olgla <elenagladenko@gmail.com>
2023-11-05 17:28:40 -0500

    Added Class AdditionalUtils

---
+++ src/test/java/school/redrover/AdditionalUtils.java
@@ -0,0 +1,28 @@
+package school.redrover;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import school.redrover.runner.BaseTest;
+
+import java.util.UUID;
+
+public class AdditionalUtils extends BaseTest {
+
+    public static String generateRandomName() {
+        String randomName = UUID.randomUUID()
+                .toString()
+                .substring(0, 5);
+        return randomName;
+    }
+
+    public void jsClick(WebElement element) {
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].click();", element);
+    }
+
+    public String findTextInPseudoElement(WebElement element, String pseudoElement) {
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        String script = "return window.getComputedStyle(arguments[0], arguments[1]).getPropertyValue('content');";
+        return (String) js.executeScript(script, element, pseudoElement);
+    }
+}
