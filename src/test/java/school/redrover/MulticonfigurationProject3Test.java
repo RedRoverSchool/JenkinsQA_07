b319c601d118aebf46b0b75d013e2197cf4181fa
TetianaBalchunas <tat.aleta@gmail.com>
2023-11-06 22:50:52 -0500

    TC_13.001.06 Resolve conflict

--- src/test/java/school/redrover/MulticonfigurationProject3Test.java
+++ src/test/java/school/redrover/MulticonfigurationProject3Test.java
@@ -23,17 +23,7 @@ public class MulticonfigurationProject3Test extends BaseTest {
         Assert.assertEquals
                 (getDriver().findElement(By.cssSelector("h1[class='matrix-project-headline page-headline']")).getText(),
                         "Project Multiconfiguration");
-    final static private String NAME_PROJECT = "TestName";
 
-    @Test
-    public void testCreateMultiProject(){
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(NAME_PROJECT);
-        getDriver().findElement(By.xpath("//li[@class='hudson_matrix_MatrixProject']")).click();
-        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
-        getDriver().findElement(By.name("Submit")).click();
-
-        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(), "Project " + NAME_PROJECT);
 
     }
 }
