52c5144593e38f036202dee989b5252d13080332
Aleksandr Altukhov <altukhovav@gmail.com>
2023-11-07 21:36:46 +0300

    TC_05.002.04

--- src/test/java/school/redrover/MultibranchPipeline4Test.java
+++ src/test/java/school/redrover/MultibranchPipeline4Test.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -27,6 +28,41 @@ public class MultibranchPipeline4Test extends BaseTest {
 
     private void goMultibranchPipelinePage() {
         getDriver().findElement(By.xpath("//span[normalize-space()='" + NAME + "']")).click();
+    }
+
+    @Test
+    public void testErrorForUnsafeChar() {
+        createMultibranchPipelin(NAME);
+        getDashboardLink();
+        goMultibranchPipelinePage();
+
+        getDriver().findElement(By.xpath("//div[8]/span/a")).click();
+
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input validated  ']")).clear();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input validated  ']")).sendKeys(RENAMED + "!");
+
+        Actions actions = new Actions(getDriver());
+        WebElement element = getDriver().findElement(By.xpath("//input[@class='jenkins-input validated  ']"));
+        actions.moveToElement(element).moveByOffset(100,50).click().build().perform();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class='error']")).getText(), "‘!’ is an unsafe character");
+    }
+
+    @Test
+    public void testRenameUsingBreadcrumb() {
+        createMultibranchPipelin(NAME);
+
+        WebElement chevron = getDriver().findElement(By.xpath("//a[@class='model-link'][text()='Multibranch Pipeline']"));
+        new Actions(getDriver()).moveToElement(chevron).build().perform();
+
+        getDriver().findElement(By.xpath("(//button[@class='jenkins-menu-dropdown-chevron'])[3]")).click();
+        getDriver().findElement(By.xpath("//div/a[6]")).click();
+
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input validated  ']")).clear();
+        getDriver().findElement(By.xpath("//input[@class='jenkins-input validated  ']")).sendKeys(RENAMED);
+        getDriver().findElement(By.xpath("//button[@class='jenkins-button jenkins-button--primary ']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), RENAMED);
     }
 
     @Test
