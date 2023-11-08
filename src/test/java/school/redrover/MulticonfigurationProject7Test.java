96a4f02385cf4b66b840803cc70714faa2ced2e3
Michael <m.cmsrus@gmail.com>
2023-11-08 20:29:17 +0100

    Fixed comments

--- src/test/java/school/redrover/MulticonfigurationProject7Test.java
+++ src/test/java/school/redrover/MulticonfigurationProject7Test.java
@@ -16,13 +16,11 @@ public class MulticonfigurationProject7Test extends BaseTest {
         getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
         getDriver().findElement(By.xpath("/html/body/div[2]/ol/li[1]/a")).click();
 
-        //first assert — To see the created project on the Dashboard;
         Assert.assertEquals(getDriver().findElement
                 (By.cssSelector("#job_hero\\ builds > td:nth-child(3) > a > span")).getText(), PROJECT_NAME);
 
         getDriver().findElement(By.xpath("//a[@class='jenkins-table__link model-link inside']")).click();
 
-        //The last one assert — Navigate to the configuration page of the created project Multiconfiguration;
         Assert.assertEquals(getDriver().findElement
                 (By.xpath("//h1")).getText(), "Project " + PROJECT_NAME);
     }
