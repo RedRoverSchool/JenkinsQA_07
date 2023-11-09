e58904ae561e7097ea4328f46aeaef10b20ded45
IvanGoncharovv <kyuvcpavm@mail.ru>
2023-11-09 15:44:12 +0700

    TC_13.001.07 | User > Create User

---
+++ src/test/java/school/redrover/User3Test.java
@@ -0,0 +1,32 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class User3Test extends BaseTest {
+
+    private static final String NAME = "ivan";
+    @Test
+    public void testCreateUser() {
+        getDriver().findElement(By.xpath("//a[@href = '/manage']")).click();
+        getDriver().findElement(By.xpath("//a[@href = 'securityRealm/']")).click();
+
+        getDriver().findElement(By.xpath("//a[@href = 'addUser']")).click();
+
+        getDriver().findElement(By.id("username")).sendKeys(NAME);
+        getDriver().findElement(By.name("password1")).sendKeys("qweqwe12");
+        getDriver().findElement(By.name("password2")).sendKeys("qweqwe12");
+        getDriver().findElement(By.name("email")).sendKeys("hotmail@hotmail.ru");
+        getDriver().findElement(By.xpath("//div[@id='bottom-sticker']//button")).click();
+
+        getDriver().findElement(By.xpath("//a[contains(@href,'" + NAME + "')]")).click();
+        getDriver().findElement(By.xpath("//a[contains(@href,'user/" + NAME + "/configure')]")).click();
+
+        Assert.assertEquals(
+                getDriver().findElement(By.name("_.fullName")).getAttribute("value"), NAME
+        );
+    }
+
+}
