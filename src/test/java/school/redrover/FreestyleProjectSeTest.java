8c0827c8cc01e6f374daa4943106770a57ee505f
jokerSharp <137673317+jokerSharp@users.noreply.github.com>
2023-11-15 16:08:10 +0300

    TC_01.002.18 | Freestyle project > Rename project > Valid title Added 1 test to rename a freestyle project on the dashboard

--- src/test/java/school/redrover/FreestyleProjectSeTest.java
+++ src/test/java/school/redrover/FreestyleProjectSeTest.java
@@ -198,17 +198,20 @@ public class FreestyleProjectSeTest extends BaseTest {
         final String projectName = "FSproject";
         final String projectRename = "FSproject1";
 
+        //span[text()='" + projectName + "']/button
+
         createFreeStyleProject(projectName);
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
-        hoverClick("//button[@data-href='http://localhost:8080/job/" + projectName + "/']");
+//        hoverClick("//button[@data-href='http://localhost:8080/job/" + projectName + "/']");
+        hoverClick("//a[@href='job/" + projectName + "/']/button");
         hoverClick("//a[@href='/job/" + projectName + "/confirm-rename']");
         getDriver().findElement(By.xpath("//input[@name='newName']")).clear();
         hoverClickInput("//input[@name='newName']", projectRename);
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
 
-        Assert.assertTrue(getDriver().findElement(By.xpath("//button[@data-href='http://localhost:8080/job/" + projectRename + "/']")).isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='" + projectRename + "']")).isDisplayed());
 
     }
 }
