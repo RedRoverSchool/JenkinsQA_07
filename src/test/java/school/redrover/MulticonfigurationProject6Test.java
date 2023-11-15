a794d80f855655265657e6f475f871ce4e18fe7a
Olga Forostinko <oforostinko@gmail.com>
2023-11-15 10:47:55 -0800

    Addeed test with description

--- src/test/java/school/redrover/MulticonfigurationProject6Test.java
+++ src/test/java/school/redrover/MulticonfigurationProject6Test.java
@@ -1,9 +1,12 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+
+import java.nio.file.Watchable;
 
 public class MulticonfigurationProject6Test extends BaseTest {
 
@@ -14,22 +17,66 @@ public class MulticonfigurationProject6Test extends BaseTest {
     private static final By SAVE_BUTTON = By.xpath("//button[@name='Submit']");
     private static final By JENKINS_LINK = By.cssSelector("#jenkins-name-icon");
     private static final By PROJECT_NAME = By.xpath("//span[contains(text(),'ProjectMulticonfiguration')]");
+    private static final By   ADD_DESCRIPTION_LINK = By.xpath("//a[@id='description-link']");
+    private static final By DESCRIPTION_INPUT = By.xpath("//textarea[@name='description']");
+    private static final By SAVE_DESCRIPTION_BUTTON = By.xpath("//button[@class='jenkins-button jenkins-button--primary ']");
+    private static final By DESCRIPTION = By.xpath("//div[@id='description']/div[1]");
+
+
+        public void createProjectMulticonfiguration() {
+        final String projectName = "ProjectMulticonfiguration";
+
+        WebElement newItemLink = getDriver().findElement(NEW_ITEM_LINK);
+        newItemLink.click();
+
+        WebElement newItemNameInput = getDriver().findElement(ITEM_NAME_INPUT);
+
+        newItemNameInput.sendKeys(projectName);
+
+        WebElement newMultiConfigurationProjectTab =   getDriver().findElement(MULTICONFIGURATION_PROJECT_TAB);
+        newMultiConfigurationProjectTab.click();
+
+        WebElement newOkButton = getDriver().findElement(OK_BUTTON);
+        newOkButton.click();
+
+        WebElement newSaveButton = getDriver().findElement(SAVE_BUTTON);
+        newSaveButton.click();
+
+        WebElement newJencinsLink = getDriver().findElement(JENKINS_LINK);
+        newJencinsLink.click();
+
+        WebElement newProjectName = getDriver().findElement(PROJECT_NAME);
+        newProjectName.click();
+    }
 
     @Test
     public void testCreate() {
 
-        getDriver().findElement(NEW_ITEM_LINK).click();
-        getDriver().findElement(ITEM_NAME_INPUT).sendKeys("ProjectMulticonfiguration");
-        getDriver().findElement(MULTICONFIGURATION_PROJECT_TAB).click();
-        getDriver().findElement(OK_BUTTON).click();
-
-        getDriver().findElement(SAVE_BUTTON).click();
-        getDriver().findElement(JENKINS_LINK).click();
-
-        getDriver().findElement(PROJECT_NAME).click();
+        createProjectMulticonfiguration();
 
         Assert.assertEquals(
                 getDriver().findElement(By.cssSelector("#main-panel > h1")).getText(),
                 "Project ProjectMulticonfiguration");
     }
+
+    @Test
+    public void testAddDescription() {
+
+        createProjectMulticonfiguration();
+        final String desriptionText = "Testing";
+
+        WebElement addDescription = getDriver().findElement(ADD_DESCRIPTION_LINK);
+        addDescription.click();
+        WebElement descriptionInput = getDriver().findElement(DESCRIPTION_INPUT);
+        descriptionInput.click();
+        descriptionInput.sendKeys(desriptionText);
+        WebElement saveDescription = getDriver().findElement(SAVE_DESCRIPTION_BUTTON);
+        saveDescription.click();
+
+        Assert.assertEquals(getDriver().findElement(DESCRIPTION).getText(), desriptionText);
+    }
 }
+
+
+
+
