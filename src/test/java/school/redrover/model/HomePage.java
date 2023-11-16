e7294b28f300c33c83f44476fa6097dcda98711e
Azat Abrarov <azat1993@gmail.com>
2023-11-16 14:46:01 +0300

    RF_15.001.05 (POM)

--- src/test/java/school/redrover/model/HomePage.java
+++ src/test/java/school/redrover/model/HomePage.java
@@ -3,11 +3,15 @@ package school.redrover.model;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BasePage;
 
 import java.util.List;
 
 public class HomePage extends BasePage {
+
+    @FindBy(xpath = "//a[@href='computer/new']")
+    private WebElement SetUpAnAgent;
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -25,4 +29,11 @@ public class HomePage extends BasePage {
 
         return resultList;
     }
+
+    public NodeCreatePage clickSetUpAnAgent() {
+        SetUpAnAgent.click();
+
+        return new NodeCreatePage(getDriver());
+    }
+
 }
