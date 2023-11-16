19322accb117a966517925d4749978f2310f42ab
Elena Sosnitsky <sosnitsky.e@gmail.com>
2023-11-16 14:51:53 +0200

    RF_01.006.001 |The tooltip message is displayed near the checkbox 'Discard old builds'(POM)

--- src/test/java/school/redrover/model/HomePage.java
+++ src/test/java/school/redrover/model/HomePage.java
@@ -3,13 +3,14 @@ package school.redrover.model;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BasePage;
 
 import java.util.List;
 
 public class HomePage extends BasePage {
 
-    @FindBy(xpath = "//a[@href='computer/new']")
+   @FindBy(xpath = "//a[@href='computer/new']")
     private WebElement SetUpAnAgent;
 
     public HomePage(WebDriver driver) {
