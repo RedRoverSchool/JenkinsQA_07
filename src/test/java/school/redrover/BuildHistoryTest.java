8605323032562dbca1196669453d17f681179160
vlad1kek <streelok10@gmail.com>
2023-11-19 00:48:31 +0200

    delete trash

--- src/test/java/school/redrover/BuildHistoryTest.java
+++ src/test/java/school/redrover/BuildHistoryTest.java
@@ -1,11 +1,8 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.model.BuildHistoryPage;
 import school.redrover.model.FreestyleProjectConfigurePage;
