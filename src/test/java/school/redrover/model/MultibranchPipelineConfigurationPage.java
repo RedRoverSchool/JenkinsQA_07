bedd0c6a760373fb007fda1c3f3b13b89b7400b1
ivs2000 <sigorkherson@gmail.com>
2023-11-17 23:54:45 -0500

    TC_09.001.04 Manage Jenkins Create User Empty Fields Test added  and new Class created

--- src/test/java/school/redrover/model/MultibranchPipelineConfigurationPage.java
+++ src/test/java/school/redrover/model/MultibranchPipelineConfigurationPage.java
@@ -1,5 +1,7 @@
 package school.redrover.model;
 
+import org.bouncycastle.pqc.crypto.util.PQCOtherInfoGenerator;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,6 +12,15 @@ public class MultibranchPipelineConfigurationPage extends BasePage {
     @FindBy(xpath = "//a[@class='model-link'][contains(@href, 'job')]")
     private WebElement breadcrumbJobName;
 
+    @FindBy(xpath = "//div[@class ='setting-main']/input")
+    private WebElement nameField;
+
+    @FindBy(xpath = "//*[@id='bottom-sticker']/div/button")
+    private WebElement buttonSubmit;
+
+    @FindBy (xpath = "//h1")
+    private WebElement nameH1;
+
     public MultibranchPipelineConfigurationPage(WebDriver driver) {
         super(driver);
     }
@@ -18,5 +29,27 @@ public class MultibranchPipelineConfigurationPage extends BasePage {
 
         return breadcrumbJobName.getText();
     }
+    public MultibranchPipelineConfigurationPage confirmRename(String name) {
+        getDriver().findElement(By.xpath("//a[@href='/job/" + name + "/confirm-rename']")).click();
+        return this;
+    }
 
+    public  MultibranchPipelineConfigurationPage clearField() {
+        nameField.clear();
+        return this;
+    }
+
+    public MultibranchPipelineConfigurationPage inputName(String name) {
+        nameField.sendKeys(name);
+        return this;
+    }
+
+    public MultibranchPipelineConfigurationPage buttonSubmit() {
+        buttonSubmit.click();
+        return this;
+    }
+
+    public String headerName() {
+        return nameH1.getText();
+    }
 }
