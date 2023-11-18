27ac90ccf065903224beb45d07d607da7432fb5b
MarinaAvr <marinakhudoiarova@gmail.com>
2023-11-18 01:54:14 -0600

    checkTippyBox() Ignored

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -113,6 +113,7 @@ public class FooterTest extends BaseTest {
                 .getText().contains("Version 2.414.2"));
     }
 
+    @Ignore
     @Test
     public void checkTippyBox() throws InterruptedException {
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);",
