f179f9a18e9750839d7694d76e499e90e9e50d3a
Korelov Maksim <korelov@gmail.com>
2023-11-21 23:40:58 +0300

    Ignore  testDeleteCustomLogRecorder

--- src/test/java/school/redrover/SystemLogTest.java
+++ src/test/java/school/redrover/SystemLogTest.java
@@ -9,7 +9,10 @@ import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class SystemLogTest extends BaseTest {
 
@@ -30,7 +33,7 @@ public class SystemLogTest extends BaseTest {
 
         getDriver().findElement(By.xpath("//a[@href='new']")).click();
         new Actions(getDriver()).moveToElement(getDriver()
-                .findElement(By.cssSelector("input[checkurl='checkNewName']")))
+                        .findElement(By.cssSelector("input[checkurl='checkNewName']")))
                 .click()
                 .perform();
         getDriver().findElement(By.cssSelector("input[checkurl='checkNewName']")).sendKeys(SYSLOG_NAME);
@@ -44,6 +47,7 @@ public class SystemLogTest extends BaseTest {
                 .getText(), SYSLOG_NAME);
     }
 
+    @Ignore
     @Test(dependsOnMethods = "testCreateCustomLogRecorder")
     public void testDeleteCustomLogRecorder() {
         List<WebElement> lst = new ArrayList<>();
@@ -55,7 +59,7 @@ public class SystemLogTest extends BaseTest {
             Actions actions = new Actions(getDriver());
             actions.pause(400)
                     .moveToElement(getDriver()
-                    .findElement(By.xpath("//a[@data-post='true']")))
+                            .findElement(By.xpath("//a[@data-post='true']")))
                     .click()
                     .perform();
             getWait5().until(ExpectedConditions.alertIsPresent()).accept();
@@ -63,7 +67,7 @@ public class SystemLogTest extends BaseTest {
             lst = getDriver().findElements(By.className("jenkins-table__link"));
             getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]/a")).click();
         } while (lst.size() > 1);
-        Assert.assertEquals(lst.size(),1);
+        Assert.assertEquals(lst.size(), 1);
     }
 
     @Test
@@ -72,39 +76,39 @@ public class SystemLogTest extends BaseTest {
         openSyslogPage();
 
         lst = getDriver().findElements(By.className("jenkins-table__link"));
-        if(lst.size() > 1) {
+        if (lst.size() > 1) {
             Iterator<WebElement> it = lst.iterator();
             while (it.hasNext()) {
                 WebElement wb = it.next();
-                    if (!wb.getText().equals( "All Jenkins Logs")){
-                        wb.click();
-                        getDriver().findElement(By.xpath("//button[@tooltip='More actions']")).click();
-                        Actions actions = new Actions(getDriver());
-                        actions.pause(400)
+                if (!wb.getText().equals("All Jenkins Logs")) {
+                    wb.click();
+                    getDriver().findElement(By.xpath("//button[@tooltip='More actions']")).click();
+                    Actions actions = new Actions(getDriver());
+                    actions.pause(400)
                             .moveToElement(getDriver()
-                                .findElement(By.xpath("//a[@data-post='true']")))
+                                    .findElement(By.xpath("//a[@data-post='true']")))
                             .click()
                             .perform();
-                        getWait5().until(ExpectedConditions.alertIsPresent()).accept();
-                        getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]/a")).click();
-                        lst = getDriver().findElements(By.className("jenkins-table__link"));
-                    } else if (lst.size() > 1) {
-                        lst.get(1).click();
-                        getDriver().findElement(By.xpath("//button[@tooltip='More actions']")).click();
-                        Actions actions = new Actions(getDriver());
-                        actions.pause(400)
+                    getWait5().until(ExpectedConditions.alertIsPresent()).accept();
+                    getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]/a")).click();
+                    lst = getDriver().findElements(By.className("jenkins-table__link"));
+                } else if (lst.size() > 1) {
+                    lst.get(1).click();
+                    getDriver().findElement(By.xpath("//button[@tooltip='More actions']")).click();
+                    Actions actions = new Actions(getDriver());
+                    actions.pause(400)
                             .moveToElement(getDriver()
-                                .findElement(By.xpath("//a[@data-post='true']")))
+                                    .findElement(By.xpath("//a[@data-post='true']")))
                             .click()
                             .perform();
-                        getWait5().until(ExpectedConditions.alertIsPresent()).accept();
-                        getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]/a")).click();
-                        lst = getDriver().findElements(By.className("jenkins-table__link"));
-                    }
+                    getWait5().until(ExpectedConditions.alertIsPresent()).accept();
+                    getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]/a")).click();
+                    lst = getDriver().findElements(By.className("jenkins-table__link"));
+                }
                 if (lst.size() == 1) break;
                 it = lst.iterator();
             }
         }
-        Assert.assertEquals(lst.size(),1);
+        Assert.assertEquals(lst.size(), 1);
     }
 }
