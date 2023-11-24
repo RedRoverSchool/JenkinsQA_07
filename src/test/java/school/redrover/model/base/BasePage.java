167643749fec8fd0323f4c1b4331fe8fbc814fd7
Azat Abrarov <azat1993@gmail.com>
2023-11-24 20:08:52 +0300

    fix

--- src/test/java/school/redrover/model/base/BasePage.java
+++ src/test/java/school/redrover/model/base/BasePage.java
@@ -48,7 +48,7 @@ public abstract class BasePage extends BaseModel {
     }
 
     public HomePage clickUserDropdown() {
-        new Actions(getDriver()).moveToElement(user, 30, 0).click().perform();
+        new Actions(getDriver()).moveToElement(user, 45, 0).click().perform();
 
 
         return new HomePage(getDriver());
