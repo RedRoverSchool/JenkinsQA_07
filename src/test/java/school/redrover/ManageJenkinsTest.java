df2c88e288b561550e1908afdbfd3c5154217c04
Azat Abrarov <azat1993@gmail.com>
2023-11-25 16:42:58 +0300

    TC_09.002.03, TC_09.002.04, TC_09.002.05

--- src/test/java/school/redrover/ManageJenkinsTest.java
+++ src/test/java/school/redrover/ManageJenkinsTest.java
@@ -4,7 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.model.HomePage;
 import school.redrover.model.ManageJenkinsPage;
+import school.redrover.model.NodeDetailsPage;
+import school.redrover.model.NodesListPage;
 import school.redrover.runner.BaseTest;
+
+import java.util.List;
 
 public class ManageJenkinsTest extends BaseTest {
 
@@ -19,5 +23,44 @@ public class ManageJenkinsTest extends BaseTest {
 
             Assert.assertEquals(manageJenkinsPage.getTooltipText(), TOOLTIP);
             Assert.assertTrue(manageJenkinsPage.shortcutTooltipIsVisible(), TOOLTIP + " is not visible");
+    }
+
+    @Test
+    public void testNoResultsTextVisibility() {
+
+        String resultText = new HomePage(getDriver())
+                .clickManageJenkins()
+                .typeSearchInputField("Test")
+                .getNoResultText();
+
+        Assert.assertEquals(resultText, "No results");
+
+    }
+
+    @Test
+    public void testRedirectPage() {
+        final String request = "Nodes";
+
+        String url = new HomePage(getDriver())
+                .clickManageJenkins()
+                .typeSearchInputField(request)
+                .clickResult(request, new NodesListPage(getDriver()))
+                .getCurrentURL();
+
+        Assert.assertTrue(url.contains("manage/computer/"));
+    }
+
+    @Test
+    public void testListOfResultsVisibility() {
+
+        List<String> result = new HomePage(getDriver())
+                .clickManageJenkins()
+                .typeSearchInputField("N")
+                .getResultsList();
+
+        Assert.assertNotEquals(
+                List.of("Plugins", "Nodes", "Credentials", "Credential", "Providers", "System Information"),
+                result
+        );
     }
 }
