36c153955ccc6120a4a8c882fbc0d9b2b1dc1b5a
lelik <lelik_gal@mail.ru>
2023-11-27 12:00:53 +0300

    Refactoring In POM Creating new item > Creating Freestyle project |TC_ 00.001.06| and Freestyle project > Delete project |TC_01.003.14 |

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -147,6 +147,7 @@ public class FreestyleProjectTest extends BaseTest {
                 .click()
                 .perform();
     }
+
     private void configureParameterizedBuild(String projectName, String choiceName, String choiceOptions) {
         getDriver().findElement(By.xpath("//a[@href='job/" + projectName + "/']")).click();
         getDriver().findElement(By.xpath("//a[@href='/job/" + projectName + "/configure']")).click();
@@ -609,6 +610,7 @@ public class FreestyleProjectTest extends BaseTest {
 
         Assert.assertTrue(thisProjectIsParameterizedCheckbox.isSelected());
     }
+
     @Ignore
     @Test
     public void testOldBuildsAreDiscarded() {
@@ -1227,4 +1229,25 @@ public class FreestyleProjectTest extends BaseTest {
 
         Assert.assertEquals(itemsActual, itemsExpected);
     }
+    @Test
+    public void testCreateNewFreestyleProject() {
+        HomePage homePage = new HomePage(getDriver())
+                .clickNewItem()
+                .typeItemName(PROJECT_NAME)
+                .selectFreestyleProject()
+                .clickOk(new FreestyleProjectDetailsPage(getDriver()))
+                .goHomePage();
+
+        Assert.assertTrue(homePage.getJobList().contains(PROJECT_NAME));
+    }
+    @Test(dependsOnMethods = "testCreateNewFreestyleProject")
+    public void testDeleteFreestyleProject() {
+        HomePage home = new HomePage(getDriver())
+                .clickJobByName(PROJECT_NAME, new FreestyleProjectDetailsPage(getDriver()))
+                .clickDeleteProject()
+                .clickAlertDeleteTheProject();
+
+        Assert.assertEquals(home.getJobList().size(), 0);
+    }
+
 }
