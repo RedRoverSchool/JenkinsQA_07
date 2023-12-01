c28b8be7da205fcc7111c44bf1d9366717931e80
Denis Prokofyev <denisprokofyev@gmail.com>
2023-12-01 14:44:18 +0300

    TC_09.008.07, 10 | Add ignore to testAddBooleanParameterDropdownIsSortedAlphabetically

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -840,6 +840,7 @@ public class FreestyleProjectTest extends BaseTest {
                 configurePage.getParameterDescription().equals(DESCRIPTION));
     }
 
+    @Ignore
     @Test(dependsOnMethods = "testCreateFreestyleProjectWithValidName")
     public void testAddBooleanParameterDropdownIsSortedAlphabetically() {
         List<String> expectedResult = List.of(
