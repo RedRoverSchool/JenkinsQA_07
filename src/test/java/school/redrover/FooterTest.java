7c1603ca788e0a6d13f46a2968b0661c2efbd527
vlad1kek <streelok10@gmail.com>
2023-12-03 00:57:17 +0200

    ref

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -11,7 +11,6 @@ import school.redrover.runner.BaseTest;
 import java.util.List;
 
 public class FooterTest extends BaseTest {
-
     private static final String JENKINS_VERSION = "Jenkins 2.414.2";
     private static final String ABOUT_JENKINS_VERSION = "Version 2.414.2";
 
